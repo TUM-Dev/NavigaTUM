f8c1d4ec2bf85053aff3ddeecdaddc7410453006
Frank Elsinga <frank@elsinga.de>
2022-01-25 13:40:54 +0100

    added temporary meilesearch configuration

--- server/deployment/configure_MeileSearch.bash
+++ server/deployment/configure_MeileSearch.bash
@@ -5,9 +5,9 @@ ROOMFINDER_URL="https://new.roomfinder.tum.sexy"
 # fail on first error
 set -e
 
-# generate a temporary WORK_DIR inside the current working directory
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-WORK_DIR=`mktemp -d -p "$DIR"`
+# generate a temporary WORK_DIR
+WORK_DIR=$(mktemp -d -t tmp_configure_meilesearch.XXXXXX)
+echo "WORK_DIR=${WORK_DIR}"
 
 # check if tmp dir was created
 if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
@@ -18,7 +18,7 @@ fi
 # deletes the temp directory on exit
 function cleanup {      
   rm -rf "$WORK_DIR"
-  echo "Deleted temp working directory $WORK_DIR"
+  echo -e "\nDeleted temp working directory $WORK_DIR"
 }
 trap cleanup EXIT
 
@@ -27,19 +27,24 @@ echo "----"
 echo "configure Meilesearch"
 echo "----"
 
+echo -e "\n> Set primary-key\n"
 curl --silent -i -X POST 'http://127.0.0.1:7700/indexes' --header 'content-type: application/json' --data '{ "uid": "entries", "primaryKey": "ms_id" }'
 
-echo "\nSet filterable attributes"
-curl --silent -X POST 'http://localhost:7700/indexes/entries/settings/filterable-attributes' --data '["facet"]'
+echo -e "\n\n> Set filterable attributes\n"
+curl --silent -X POST 'http://localhost:7700/indexes/entries/settings/filterable-attributes' --header 'content-type: application/json' --data '["facet"]'
 
 
-# get the search_data from the cdn
-echo "\nDownloading search data to '$WORK_DIR\search_data.json'"
-wget --directory-prefix $WORK_DIR $ROOMFINDER_URL/cdn/search_data.json
-echo "\nUpload entries data"
-curl --silent -i -X PUT 'http://127.0.0.1:7700/indexes/entries/documents' --header 'content-type: application/json' --data-binary @$WORK_DIR\search_data.json
+SEARCH_DATA_FILE=$WORK_DIR/search_data.json
+echo -e "\n\n> Downloading search data to '$SEARCH_DATA_FILE'\n"
+#wget --directory-prefix $WORK_DIR $ROOMFINDER_URL/cdn/search_data.json
+cp /search_data.json $WORK_DIR
+echo "SEARCH_DATA_FILE ($SEARCH_DATA_FILE):"
+ls -lah $SEARCH_DATA_FILE
+echo -e "\n> Upload entries data:\n"
+echo @$WORK_DIR/search_data.json
+curl --silent -i -X PUT 'http://127.0.0.1:7700/indexes/entries/documents' --header 'content-type: application/json' --data-binary @$SEARCH_DATA_FILE
 
-echo "\nConfigure index"
-curl --silent -X POST 'http://localhost:7700/indexes/entries/settings/ranking-rules' --data '["words","typo","rank:desc","exactness","proximity","attribute"]'
-#curl --silent -X POST 'http://localhost:7700/indexes/entries/settings/synonyms' --data @../navigatum-server/search_synonyms.json
-curl --silent -X POST 'http://localhost:7700/indexes/entries/settings/searchable-attributes' --data '[ "ms_id", "name", "arch_name", "type", "type_common_name", "parent_building", "parent_keywords", "address", "usage" ]'
+echo -e "\n\n> Configure index:\n"
+curl --silent -X POST 'http://localhost:7700/indexes/entries/settings/ranking-rules' --header 'content-type: application/json' --data '["words","typo","rank:desc","exactness","proximity","attribute"]'
+#curl --silent -X POST 'http://localhost:7700/indexes/entries/settings/synonyms' --header 'content-type: application/json' --data @../navigatum-server/search_synonyms.json
+curl --silent -X POST 'http://localhost:7700/indexes/entries/settings/searchable-attributes' --header 'content-type: application/json' --data '[ "ms_id", "name", "arch_name", "type", "type_common_name", "parent_building", "parent_keywords", "address", "usage" ]'
