491d60d2c7311816d03cd364c8652903b0e60548
Frank Elsinga <frank@elsinga.de>
2022-02-06 16:15:27 +0100

    added the inital draft of the custom maps style current state is not integrated into the build system

--- webclient/src/modules/interactive-map.js
+++ webclient/src/modules/interactive-map.js
@@ -22,26 +22,47 @@ navigatum.registerModule("interactive-map", (function() {
                 el_css.rel = "stylesheet";
                 el_css.href = "/* @echo app_prefix */css/leaflet-1.7.1-with-plugins.css";
                 head.appendChild(el_css);
+
+                var mb_css  = document.createElement("link");
+                mb_css.rel = "stylesheet";
+                mb_css.href = "https://api.tiles.mapbox.com/mapbox-gl-js/v1.2.0/mapbox-gl.css";
+                head.appendChild(mb_css);
+
                 // JS should trigger init on load
                 var el_js = document.createElement("script");
                 el_js.src = "/* @echo app_prefix */js/leaflet-1.7.1-with-plugins.min.js";
                 el_js.onload = function() {
                     initLeaflet(_this);
-                    resolve();
+                    var mb_js = document.createElement("script");
+                    mb_js.src = "https://api.tiles.mapbox.com/mapbox-gl-js/v1.2.0/mapbox-gl.js";
+                    mb_js.onload = function() {
+                        var mbl_js = document.createElement("script");
+                        mbl_js.src = "https://cdnjs.cloudflare.com/ajax/libs/mapbox-gl-leaflet/0.0.15/leaflet-mapbox-gl.js";
+                        mbl_js.onload = function() {
+                            resolve();
+                        }
+                        head.appendChild(mbl_js);
+                    }
+                    head.appendChild(mb_js);
                 }
                 head.appendChild(el_js);
             });
         },
         initMap: function(id) {
-            var map = L.map('interactive-map');
+            var map = L.map('interactive-map').setView([38.912753, -77.032194], 15);
             // Gesture handling currently only on mobile
             if (window.matchMedia &&
                 window.matchMedia("only screen and (max-width: 480px)").matches) {
                 map.gestureHandling.enable();
             }
             console.log(map);
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            var gl = L.mapboxGL({
+                antialias: true,
+                // create the gl context with MSAA antialiasing, so custom layers are antialiasing.
+                // slower, but prettier and therefore worth it for our use case
+                style: 'https://api.maptiler.com/maps/f37dd045-7438-4fff-9bc2-73fce2ba974f/style.json?key=qMbPTihz6isJJ483G8aF',
+                interactive: false, // to prevent the map from stuttering too much while rendering
+                attribution: '<a href="https://www.openstreetmap.org/copyright">&copy; OpenStreetMap contributors, CC-BY-SA</a> | <a href="https://www.maptiler.com/copyright/">&copy; MapTiler</a>',
             }).addTo(map);
             
             return map;
