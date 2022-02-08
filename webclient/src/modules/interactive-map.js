d00d53d935224697dec7f7f6b60d38cd3d8c1145
Frank Elsinga <frank@elsinga.de>
2022-02-08 01:30:56 +0100

    moved to a different mapsstyle and added proper attribution

--- webclient/src/modules/interactive-map.js
+++ webclient/src/modules/interactive-map.js
@@ -20,30 +20,15 @@ navigatum.registerModule("interactive-map", (function() {
                 // Add CSS first (required by Leaflet)
                 var el_css  = document.createElement("link");
                 el_css.rel = "stylesheet";
-                el_css.href = "/* @echo app_prefix */css/leaflet-1.7.1-with-plugins.css";
+                el_css.href = "/* @echo app_prefix */css/leaflet-with-plugins.css";
                 head.appendChild(el_css);
-
-                var mb_css  = document.createElement("link");
-                mb_css.rel = "stylesheet";
-                mb_css.href = "https://api.tiles.mapbox.com/mapbox-gl-js/v1.2.0/mapbox-gl.css";
-                head.appendChild(mb_css);
 
                 // JS should trigger init on load
                 var el_js = document.createElement("script");
-                el_js.src = "/* @echo app_prefix */js/leaflet-1.7.1-with-plugins.min.js";
+                el_js.src = "/* @echo app_prefix */js/leaflet-with-plugins.min.js";
                 el_js.onload = function() {
                     initLeaflet(_this);
-                    var mb_js = document.createElement("script");
-                    mb_js.src = "https://api.tiles.mapbox.com/mapbox-gl-js/v1.2.0/mapbox-gl.js";
-                    mb_js.onload = function() {
-                        var mbl_js = document.createElement("script");
-                        mbl_js.src = "https://cdnjs.cloudflare.com/ajax/libs/mapbox-gl-leaflet/0.0.15/leaflet-mapbox-gl.js";
-                        mbl_js.onload = function() {
-                            resolve();
-                        }
-                        head.appendChild(mbl_js);
-                    }
-                    head.appendChild(mb_js);
+                    resolve();
                 }
                 head.appendChild(el_js);
             });
@@ -57,14 +42,19 @@ navigatum.registerModule("interactive-map", (function() {
             }
             console.log(map);
             var gl = L.mapboxGL({
-                antialias: true,
                 // create the gl context with MSAA antialiasing, so custom layers are antialiasing.
                 // slower, but prettier and therefore worth it for our use case
-                style: 'https://api.maptiler.com/maps/f37dd045-7438-4fff-9bc2-73fce2ba974f/style.json?key=qMbPTihz6isJJ483G8aF',
-                interactive: false, // to prevent the map from stuttering too much while rendering
-                attribution: '<a href="https://www.openstreetmap.org/copyright">&copy; OpenStreetMap contributors, CC-BY-SA</a> | <a href="https://www.maptiler.com/copyright/">&copy; MapTiler</a>',
+                antialias: true,
+
+                //preview of the following style is available at
+                // https://api.mapbox.com/styles/v1/commanderstorm/ckzdc14en003m14l9l8iqwotq.html?title=copy&access_token=pk.eyJ1IjoiY29tbWFuZGVyc3Rvcm0iLCJhIjoiY2t6ZGJyNDBoMDU2ZzJvcGN2eTg2cWtxaSJ9.PY6Drc3tYHGqSy0UVmVnCg&zoomwheel=true&fresh=true#16.78/48.264624/11.670726
+                style: 'mapbox://styles/commanderstorm/ckzdc14en003m14l9l8iqwotq',
+                accessToken: 'pk.eyJ1IjoiY29tbWFuZGVyc3Rvcm0iLCJhIjoiY2t6ZGJyNDBoMDU2ZzJvcGN2eTg2cWtxaSJ9.PY6Drc3tYHGqSy0UVmVnCg',
+
+                // to prevent the map from stuttering too much while rendering
+                interactive: false,
+                attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a>'
             }).addTo(map);
-            
             return map;
         },
         icon: null,
