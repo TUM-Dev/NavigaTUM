6af16661251f38da7d346a4cd6838782d3cfe16c
Frank Elsinga <frank@elsinga.de>
2022-02-09 11:40:38 +0100

    added a NavigationControl GeolocateControl is commented out, as it needs some custom styling which is another PR

--- webclient/src/modules/interactive-map.js
+++ webclient/src/modules/interactive-map.js
@@ -40,7 +40,7 @@ navigatum.registerModule("interactive-map", (function() {
         },
         initMap: function(container_id) {
             mapboxgl.accessToken= 'pk.eyJ1IjoiY29tbWFuZGVyc3Rvcm0iLCJhIjoiY2t6ZGJyNDBoMDU2ZzJvcGN2eTg2cWtxaSJ9.PY6Drc3tYHGqSy0UVmVnCg'
-            return new mapboxgl.Map({
+            const map= new mapboxgl.Map({
                 container: container_id,
 
                 // create the gl context with MSAA antialiasing, so custom layers are antialiasing.
@@ -51,6 +51,17 @@ navigatum.registerModule("interactive-map", (function() {
                 // https://api.mapbox.com/styles/v1/commanderstorm/ckzdc14en003m14l9l8iqwotq.html?title=copy&access_token=pk.eyJ1IjoiY29tbWFuZGVyc3Rvcm0iLCJhIjoiY2t6ZGJyNDBoMDU2ZzJvcGN2eTg2cWtxaSJ9.PY6Drc3tYHGqSy0UVmVnCg&zoomwheel=true&fresh=true#16.78/48.264624/11.670726
                 style: 'mapbox://styles/commanderstorm/ckzdc14en003m14l9l8iqwotq',
             });
+            const nav = new mapboxgl.NavigationControl();
+            map.addControl(nav, 'top-left');
+            //const location = new mapboxgl.GeolocateControl({
+            //    positionOptions: {
+            //    enableHighAccuracy: true
+            //    },
+            //    trackUserLocation: true,
+            //    showUserHeading: true
+            //});
+            //map.addControl(location);
+            return map;
         },
     }
 })());
