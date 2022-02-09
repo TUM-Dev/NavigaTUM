19c0f961f436e5e80a623afa537e1e76aebdc836
Frank Elsinga <frank@elsinga.de>
2022-02-09 11:05:15 +0100

    implemented custom image as marker

--- webclient/config-local.js
+++ webclient/config-local.js
@@ -6,8 +6,8 @@ var config = {
     // hash-based navigation in development builds.
     app_prefix: "",
     // Prefix for 'cdn' content, e.g. images.
-    cdn_prefix: "/cdn/",
+    cdn_prefix: "https://roomapi.tum.sexy/cdn/",
     // Prefix for API requests
-    api_prefix: "http://localhost:8080/api/",
+    api_prefix: "https://roomapi.tum.sexy/api/",
 };
 module.exports = config;
