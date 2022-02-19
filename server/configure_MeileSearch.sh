bce33c5277833a0102762f9f9c44725811907426
Frank Elsinga <frank@elsinga.de>
2022-02-19 15:34:47 +0100

    potentially fixed an MeileSearch-service issue where the service did not start up properly

--- server/configure_MeileSearch.sh
+++ server/configure_MeileSearch.sh
@@ -5,29 +5,32 @@ set -e # fail on first error
 echo "----"
 echo "configure MeiliSearch"
 echo "----"
+export CURL_ERROR_ARGS=--connect-timeout 5 --max-time 10 --retry 10 --retry-delay 5 --retry-max-time 60 --retry-connrefused
+export CURL_COMMON_ARGS=$CURL_ERROR_ARGS --header 'content-type: application/json' -i
 
 echo
 echo "> Set primary-key"
 echo
-curl -i -X POST 'http://127.0.0.1:7700/indexes' --header 'content-type: application/json' --data '{ "uid": "entries", "primaryKey": "ms_id" }'
+curl $CURL_COMMON_ARGS --request POST 'http://localhost:7700/indexes' --data '{ "uid": "entries", "primaryKey": "ms_id" }'
 echo
 echo
 echo "> Set filterable attributes"
 echo
-curl -X POST 'http://localhost:7700/indexes/entries/settings/filterable-attributes' --header 'content-type: application/json' --data '["facet"]'
+curl $CURL_COMMON_ARGS --request POST 'http://localhost:7700/indexes/entries/settings/filterable-attributes' --data '["facet"]'
 
 echo
 echo "> Upload entries data:"
 echo
-curl -i -X PUT 'http://127.0.0.1:7700/indexes/entries/documents' --header 'content-type: application/json' --data-binary "@./search_data.json"
+ls -lah "./search_data.json"
+curl $CURL_COMMON_ARGS --request PUT 'http://localhost:7700/indexes/entries/documents' --data-binary "@./search_data.json"
 
 echo
 echo
 echo "> Configure index:"
 echo
-curl -X POST 'http://localhost:7700/indexes/entries/settings/ranking-rules' --header 'content-type: application/json' --data '["words","typo","rank:desc","exactness","proximity","attribute"]'
+curl $CURL_COMMON_ARGS --request POST 'http://localhost:7700/indexes/entries/settings/ranking-rules' --data '["words", "typo", "rank:desc", "exactness", "proximity", "attribute"]'
 
 echo "synonyms:"
-ls -lah "./search_data.json"
-curl -X POST 'http://localhost:7700/indexes/entries/settings/synonyms' --header 'content-type: application/json' --data "@./search_synonyms.json"
-curl -X POST 'http://localhost:7700/indexes/entries/settings/searchable-attributes' --header 'content-type: application/json' --data '[ "ms_id", "name", "arch_name", "type", "type_common_name", "parent_building", "parent_keywords", "address", "usage" ]'
+ls -lah "./search_synonyms.json"
+curl $CURL_COMMON_ARGS --request POST 'http://localhost:7700/indexes/entries/settings/synonyms' --data "@./search_synonyms.json"
+curl $CURL_COMMON_ARGS --request POST 'http://localhost:7700/indexes/entries/settings/searchable-attributes' --data '["ms_id", "name", "arch_name", "type", "type_common_name", "parent_building", "parent_keywords", "address", "usage"]'
