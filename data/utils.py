17ceaf1ec2e2f5e57d11d0020f20d54c21430acf
Frank Elsinga <frank@elsinga.de>
2022-03-05 21:35:02 +0100

    moved convert_to_webp to an utils file instead of including it from the downloader

---
+++ data/utils.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+from PIL import Image
+
+
+def convert_to_webp(source: str):
+    """Convert image to WebP.
+
+    Args:
+        source (pathlib.Path): Path to source image
+
+    Returns:
+        pathlib.Path: path to new image
+    """
+    destination = Path(source).with_suffix(".webp")
+
+    image = Image.open(source)
+    image.save(destination, format="webp")
+    os.remove(source)
+    return str(destination)
