21492611a7c58509d33876b4e02a28c30e834410
Frank Elsinga <frank@elsinga.de>
2022-03-29 23:33:16 +0200

    Added ability for convert_to_webp to handle directory's recursively

--- data/utils.py
+++ data/utils.py
@@ -5,15 +5,22 @@ from PIL import Image
 
 
 def convert_to_webp(source: Path):
-    """Convert image to WebP.
+    """Convert image(s) to WebP.
 
     Args:
-        source (pathlib.Path): Path to source image
+        source (pathlib.Path): Path to source image(s)
 
     Returns:
-        pathlib.Path: path to new image
+        pathlib.Path: path to new image(s)
     """
-    destination = Path(source).with_suffix(".webp")
+    if source.is_dir():
+        for img_path in os.listdir(source):
+            img_path = Path(source, img_path)
+            if img_path.suffix not in ['.webp', ".yaml"]:
+                convert_to_webp(img_path)
+        return source
+
+    destination = source.with_suffix(".webp")
 
     image = Image.open(source)
     image.save(destination, format="webp")
