3aa16f8e7fdaedad32decede4c6a4dd62b2d6c7c
octycs <git@octycs.eu>
2022-04-26 22:16:56 +0200

    Hide instead of delete & bug fixes

--- webclient/src/modules/interactive-map.js
+++ webclient/src/modules/interactive-map.js
@@ -43,7 +43,7 @@ navigatum.registerModule("interactive-map", (function() {
         },
         initMap: function(container_id) {
             mapboxgl.accessToken= 'pk.eyJ1IjoiY29tbWFuZGVyc3Rvcm0iLCJhIjoiY2t6ZGJyNDBoMDU2ZzJvcGN2eTg2cWtxaSJ9.PY6Drc3tYHGqSy0UVmVnCg'
-            const map= new mapboxgl.Map({
+            const map = new mapboxgl.Map({
                 container: container_id,
 
                 // create the gl context with MSAA antialiasing, so custom layers are antialiasing.
@@ -74,29 +74,40 @@ navigatum.registerModule("interactive-map", (function() {
             //});
             //map.addControl(location);
             
+            // Each source / style change causes the map to get
+            // into "loading" state, so map.loaded() is not reliable
+            // enough to know whether just the initial loading has
+            // succeded.
+            map.on("load", function() {
+                map.initial_loaded = true;
+            })
+
             _map = map;
             
             return map;
         },
-        setOverlayImage: function(img_url, coords) {
+        setOverlayImages: function(img_url, coords) {
             // Even if the map is initialized, it could be that
             // it hasn't loaded yet, so we need to postpone adding
             // the overlay layer.
-            if (!_map.loaded()) {
+            // However, the official `loaded()` function is a problem
+            // here, because the map is shortly in a "loading" state
+            // when source / style is changed, even though the initial
+            // loading is complete (and only the initial loading seems
+            // to be required to do changes here)
+            if (!_map.initial_loaded) {
                 var _this = this;
                 _map.on("load", function() {
-                    _this.setOverlayImage(img_url, coords);
+                    _this.setOverlayImages(img_url, coords);
                 });
                 return;
             }
             
-            if (img_url === null) {  // Remove overlay
+            if (img_url === null) {  // Hide overlay
                 if (_map.getLayer("overlay-layer"))
-                    _map.removeLayer("overlay-layer")
+                    _map.setLayoutProperty("overlay-layer", "visibility", "none")
                 if (_map.getLayer("overlay-bg"))
-                    _map.removeLayer("overlay-bg")
-                if (_map.getSource("overlay-src"))
-                    _map.removeSource("overlay-src")
+                    _map.setLayoutProperty("overlay-bg", "visibility", "none")
             } else {
                 source = _map.getSource("overlay-src");
                 if (!source) {
@@ -107,7 +118,7 @@ navigatum.registerModule("interactive-map", (function() {
                     })
                 } else {
                     source.url = img_url;
-                    source.coordinates = coors;
+                    source.coordinates = coords;
                 }
                 
                 layer = _map.getLayer("overlay-layer")
@@ -124,10 +135,13 @@ navigatum.registerModule("interactive-map", (function() {
                         "id": "overlay-layer",
                         "type": "raster",
                         "source": "overlay-src",
-                        'paint': {
-                            'raster-fade-duration': 0
+                        "paint": {
+                            "raster-fade-duration": 0,
                         }
                     })
+                } else {
+                    _map.setLayoutProperty("overlay-layer", "visibility", "visible")
+                    _map.setLayoutProperty("overlay-bg", "visibility", "visible")
                 }
             }
         },
