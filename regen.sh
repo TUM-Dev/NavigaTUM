6a822c51203a947593cb4421d33f1a747817b213
Frank Elsinga <frank@elsinga.de>
2022-05-04 17:50:52 +0200

    Added a script to regenerate all availible data. Only used for development

---
+++ regen.sh
@@ -0,0 +1,27 @@
+source venv/bin/activate
+
+echo "regenerating the data for /data"
+(
+cd ./data || exit
+python compile.py
+)
+
+
+echo "regenerating the data for /server"
+(
+cd ./server || exit
+rm -f data/*
+cp ../data/*.json data/
+cp ../data/output/*.json data/
+python load_api_data_to_db.py
+)
+
+
+echo "regenerating the data for /webclient"
+(
+cd ./webclient || exit
+rm -fr cdn
+mkdir cdn
+rsync -r --exclude '*.yaml' ../data/sources/img/ cdn/
+cp -r ../data/external/maps/roomfinder/* cdn/maps/roomfinder/webp/
+)
