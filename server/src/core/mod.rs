026d56ea7f703830becf5ed3eb5fd9d1cb9bbeb4
Frank Elsinga <frank@elsinga.de>
2022-05-08 17:33:05 +0200

    more heavily utilised the mod.rs files for storing code in less obscure places

--- server/src/core/mod.rs
+++ server/src/core/mod.rs
@@ -5,5 +5,5 @@ mod search;
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(get::get_handler);
-    cfg.service(search::search_handler::search_handler);
+    cfg.service(search::search_handler);
 }
