eaf7f6e53fd961b151e58161d6b8797924e8a209
Frank Elsinga <frank@elsinga.de>
2022-06-10 22:04:43 +0200

    refactored the areatree to immidiatly throw errors and split it into two functions
    Signed-off-by: Frank Elsinga <frank@elsinga.de>

--- data/processors/areatree.py
+++ data/processors/areatree.py
@@ -1,10 +1,5 @@
-import logging
-
-
 def read_areatree():
-    # Errors are collected up to the end of the file to show all at once
-    error = False
-
+    """Reads the areatree file and the basic data, gained from the areatree"""
     data = {}
     parent_stack = []
     last_element = None
@@ -16,74 +11,68 @@ def read_areatree():
                 continue
 
             indent = len(line) - len(line.lstrip(" "))
-            if indent % 2 == 0:
-                if (indent // 2) > len(parent_stack):
-                    parent_stack.append(last_element)
-                elif (indent // 2) < len(parent_stack):
-                    parent_stack = parent_stack[: indent // 2]
-            else:
-                error = True
-                logging.error(f"Indentation not multiple of 2: '{line}'")
-                continue
+            if indent % 2 != 0:
+                raise RuntimeError(f"Indentation not multiple of 2: '{line}'")
+            if (indent // 2) > len(parent_stack):
+                parent_stack.append(last_element)
+            elif (indent // 2) < len(parent_stack):
+                parent_stack = parent_stack[: indent // 2]
 
-            # The syntax is building-id(s):name(s):internal-id[,visible-id]
-            parts = line.split(":")
-            if len(parts) != 3:
-                error = True
-                logging.error(f"Invalid line, expected 3 ':'-separated parts: '{line}'")
-                continue
-
-            building_data = {
-                "parents": parent_stack[:],
-            }
-
-            # building id(s)
-            if "-" in parts[0]:
-                building_data["data_quality"] = {"areatree_uncertain": True}
-                parts[0] = parts[0].replace("-", "")
-
-            if "," in parts[0]:
-                building_data["b_prefix"] = parts[0].strip().split(",")
-            elif len(parts[0].strip()) > 0:
-                b_id = parts[0].strip()
-                building_data["b_prefix"] = b_id
-                building_data["id"] = b_id
-
-            # name
-            building_data["name"] = parts[1]
-
-            # id and type
-            if "[" in parts[2]:
-                building_data["type"] = parts[2].split("[")[1].strip()[:-1]
-                parts[2] = parts[2].split("[")[0]
-
-            if "," in parts[2]:
-                ids = parts[2].strip().split(",")
-                if len(ids) != 2:
-                    error = True
-                    logging.error(f"More than two ids found: '{line}'")
-                    continue
-
-                building_data["id"] = ids[0]
-                building_data["visible-id"] = ids[1]
-            elif len(parts[2].strip()) > 0:
-                building_data["id"] = parts[2].strip()
-
-            if "id" not in building_data:
-                error = True
-                logging.error(f"No id provided in line: '{line}'")
-                continue
-
-            # we infer which type some elements are, if they have not specified it
-            if "type" not in building_data:
-                if "b_prefix" in building_data and building_data["id"] == building_data["b_prefix"]:
-                    building_data["type"] = "building"
-                else:
-                    building_data["type"] = "area"
-
-            data[building_data["id"]] = building_data
-            last_element = building_data["id"]
-
-    if error:
-        raise RuntimeError("One or more errors, aborting")
+            last_element = _parse_areatree_line(line, parent_stack, data)
     return data
+
+
+def _parse_areatree_line(line, parent_stack, data):
+    """Parses a line from the areatree file to reveal the correct parent and children"""
+
+    # The syntax is building-id(s):name(s):internal-id[,visible-id]
+    parts = line.split(":")
+    if len(parts) != 3:
+        raise RuntimeError(f"Invalid line, expected 3 ':'-separated parts: '{line}'")
+
+    building_data = {
+        "parents": parent_stack[:],
+    }
+
+    # building id(s)
+    if "-" in parts[0]:
+        building_data["data_quality"] = {"areatree_uncertain": True}
+        parts[0] = parts[0].replace("-", "")
+
+    if "," in parts[0]:
+        building_data["b_prefix"] = parts[0].strip().split(",")
+    elif len(parts[0].strip()) > 0:
+        b_id = parts[0].strip()
+        building_data["b_prefix"] = b_id
+        building_data["id"] = b_id
+
+    # name
+    building_data["name"] = parts[1]
+
+    # id and type
+    if "[" in parts[2]:
+        building_data["type"] = parts[2].split("[")[1].strip()[:-1]
+        parts[2] = parts[2].split("[")[0]
+
+    if "," in parts[2]:
+        ids = parts[2].strip().split(",")
+        if len(ids) != 2:
+            raise RuntimeError(f"More than two ids found: '{line}'")
+
+        building_data["id"] = ids[0]
+        building_data["visible-id"] = ids[1]
+    elif len(parts[2].strip()) > 0:
+        building_data["id"] = parts[2].strip()
+
+    if "id" not in building_data:
+        raise RuntimeError(f"No id provided in line: '{line}'")
+
+    # we infer which type some elements are, if they have not specified it
+    if "type" not in building_data:
+        if "b_prefix" in building_data and building_data["id"] == building_data["b_prefix"]:
+            building_data["type"] = "building"
+        else:
+            building_data["type"] = "area"
+
+    data[building_data["id"]] = building_data
+    return building_data["id"]
