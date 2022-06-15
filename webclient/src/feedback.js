3cefb0d8deb39aca6b030b74c38f7b223c52ec3c
Frank Elsinga <frank@elsinga.de>
2022-06-15 22:46:51 +0200

    allowed dangling underscored and made more things contain underscores

--- webclient/src/feedback.js
+++ webclient/src/feedback.js
@@ -5,7 +5,7 @@
 const feedback = (function () {
   let token = null;
 
-  function requestPage(method, url, data, onsuccess, onerror) {
+  function _requestPage(method, url, data, onsuccess, onerror) {
     const req = new XMLHttpRequest();
     req.open(method, window.encodeURI(url), true);
     req.onload = function () {
@@ -22,13 +22,13 @@ const feedback = (function () {
     }
   }
 
-  function showError(msg, blockSend) {
+  function _showError(msg, blockSend) {
     msg = msg || "";
     document.getElementById("feedback-error").innerText = msg;
     document.getElementById("feedback-send").disabled = blockSend || false;
   }
 
-  function showLoading(isLoading) {
+  function _showLoading(isLoading) {
     if (isLoading) {
       document.getElementById("feedback-send").classList.add("loading");
       document.getElementById("feedback-send").disabled = true;
@@ -49,8 +49,8 @@ const feedback = (function () {
     document.getElementById("feedback-privacy").checked = false;
     document.getElementById("feedback-delete").checked = false;
 
-    showError(false);
-    showLoading(false);
+    _showError(false);
+    _showLoading(false);
 
     document.getElementById("feedback-modal").classList.add("active");
     document.body.classList.add("no-scroll");
@@ -61,7 +61,7 @@ const feedback = (function () {
       token = navigatum.getLocalStorageWithExpiry("feedback-token", null);
     }
     if (token === null || Date.now() - token.creation > 1000 * 3600 * 6) {
-      requestPage(
+      _requestPage(
         "POST",
         "/* @echo api_prefix */feedback/get_token",
         null,
@@ -74,17 +74,17 @@ const feedback = (function () {
             if (navigatum)
               navigatum.setLocalStorageWithExpiry("feedback-token", token, 6);
           } else if (r.status === 429) {
-            showError("${{_.feedback.error.429}}$", true);
+            _showError("${{_.feedback.error.429}}$", true);
           } else if (r.status === 503) {
-            showError("${{_.feedback.error.503}}$", true);
+            _showError("${{_.feedback.error.503}}$", true);
           } else {
             const unexpectedTokenError =
               "${{_.feedback.error.token_unexpected_status}}$";
-            showError(`${unexpectedTokenError}${r.status}`, true);
+            _showError(`${unexpectedTokenError}${r.status}`, true);
           }
         },
         function (r) {
-          showError("${{_.feedback.error.token_req_failed}}$");
+          _showError("${{_.feedback.error.token_req_failed}}$");
           console.log(r);
         }
       );
@@ -137,39 +137,39 @@ const feedback = (function () {
 
   function sendForm() {
     if (token === null) {
-      showError("${{_.feedback.error.send_no_token}}$", true);
+      _showError("${{_.feedback.error.send_no_token}}$", true);
     } else if (document.getElementById("feedback-subject").value.length < 3) {
-      showError("${{_.feedback.error.too_short_subject}}$");
+      _showError("${{_.feedback.error.too_short_subject}}$");
     } else if (document.getElementById("feedback-body").value.length < 10) {
-      showError("${{_.feedback.error.too_short_body}}$");
+      _showError("${{_.feedback.error.too_short_body}}$");
     } else if (document.getElementById("feedback-privacy").checked !== true) {
-      showError("${{_.feedback.error.privacy_not_checked}}$");
+      _showError("${{_.feedback.error.privacy_not_checked}}$");
     } else {
-      showLoading(true);
+      _showLoading(true);
       // Token may only be used after a short delay. In case that has not passed
       // yet, we wait until for a short time.
       if (Date.now() - token.creation < 1000 * 10) {
-        window.setTimeout(send, 1000 * 10 - (Date.now() - token.creation));
+        window.setTimeout(_send, 1000 * 10 - (Date.now() - token.creation));
       } else {
-        send();
+        _send();
       }
     }
   }
 
-  function showSuccess(href) {
+  function _showSuccess(href) {
     document.getElementById("feedback-modal").classList.remove("active");
     document.getElementById("feedback-success-modal").classList.add("active");
     document.getElementById("feedback-success-url").setAttribute("href", href);
   }
 
-  function send() {
+  function _send() {
     const category = document.getElementById("feedback-category").value;
     const subject = document.getElementById("feedback-subject").value;
     const body = document.getElementById("feedback-body").value;
     const privacy = document.getElementById("feedback-privacy").checked;
     const deleteIssue = document.getElementById("feedback-delete").checked;
 
-    requestPage(
+    _requestPage(
       "POST",
       "/* @echo api_prefix */feedback/feedback",
       JSON.stringify({
@@ -181,33 +181,33 @@ const feedback = (function () {
         delete_issue_requested: deleteIssue,
       }),
       function (r) {
-        showLoading(false);
+        _showLoading(false);
         if (r.status === 201) {
           localStorage.removeItem("coordinate-feedback");
           token = null;
           localStorage.removeItem("feedback-token");
           const e = new Event("storage");
           window.dispatchEvent(e);
-          showSuccess(r.responseText);
+          _showSuccess(r.responseText);
         } else if (r.status === 500) {
           const serverError = "${{_.feedback.error.server_error}}$";
-          showError(`${serverError} (${r.responseText})`, false);
+          _showError(`${serverError} (${r.responseText})`, false);
         } else if (r.status === 451) {
-          showError("${{_.feedback.error.privacy_not_checked}}$", false);
+          _showError("${{_.feedback.error.privacy_not_checked}}$", false);
         } else if (r.status === 403) {
           localStorage.removeItem("feedback-token");
           token = null;
           const invalidTokenError = "${{_.feedback.error.send_invalid_token}}$";
-          showError(`${invalidTokenError} (${r.responseText})`, false);
+          _showError(`${invalidTokenError} (${r.responseText})`, false);
         } else {
           const unexpectedStatusError =
             "${{_.feedback.error.send_invalid_token}}$";
-          showError(`${unexpectedStatusError}${r.status}`, false);
+          _showError(`${unexpectedStatusError}${r.status}`, false);
         }
       },
       function (r) {
-        showLoading(false);
-        showError("${{_.feedback.error.send_req_failed}}$");
+        _showLoading(false);
+        _showError("${{_.feedback.error.send_req_failed}}$");
         console.log(r);
       }
     );
