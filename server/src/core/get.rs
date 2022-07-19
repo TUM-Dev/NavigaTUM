13e245a5ce834ad6d3f6c4d550f77f8d841129d7
Frank Elsinga <frank@elsinga.de>
2022-07-19 18:55:57 +0200

    utilise an explicitly cached prepared statement

--- server/src/core/get.rs
+++ server/src/core/get.rs
@@ -10,7 +10,7 @@ pub async fn get_handler(params: web::Path<String>) -> HttpResponse {
     )
     .expect("Cannot open database");
     let mut stmt = conn
-        .prepare("SELECT value FROM api_data WHERE key = ?")
+        .prepare_cached("SELECT value FROM api_data WHERE key = ?")
         .expect("Cannot prepare statement");
     let result = stmt.query_row(params![id], |row| {
         let data: String = row.get_unwrap(0);
