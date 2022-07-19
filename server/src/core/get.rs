42a9e68619405c3b4d5ebf93b3dd2363cb478575
Frank Elsinga <frank@elsinga.de>
2022-07-19 18:56:27 +0200

    removed unnessesary makro usage

--- server/src/core/get.rs
+++ server/src/core/get.rs
@@ -1,5 +1,5 @@
 use actix_web::{get, web, HttpResponse};
-use rusqlite::{params, Connection, OpenFlags};
+use rusqlite::{Connection, OpenFlags};
 
 #[get("/get/{id}")]
 pub async fn get_handler(params: web::Path<String>) -> HttpResponse {
@@ -12,7 +12,7 @@ pub async fn get_handler(params: web::Path<String>) -> HttpResponse {
     let mut stmt = conn
         .prepare_cached("SELECT value FROM api_data WHERE key = ?")
         .expect("Cannot prepare statement");
-    let result = stmt.query_row(params![id], |row| {
+    let result = stmt.query_row([id], |row| {
         let data: String = row.get_unwrap(0);
         Ok(data)
     });
