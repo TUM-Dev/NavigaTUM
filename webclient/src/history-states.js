6e168622eb8183325ead1de2a2dec1d8cdd8bb1b
Frank Elsinga <frank@elsinga.de>
2022-07-31 20:00:10 +0200

    Js scss reformttings (#167)
    * Reformatted .inc and our packackage.json to have consistent 2 spaces of padding
    * changed all var's to const/let
    * removed extended data as unused
    * removed leading zeros from scss files.
    This is more consistent with the style used across most of the project
    * removed object shorthands.
    This way is more consistent across the existing codebase
    * switched from the rgba to the rgb css widget.
    Reasoning is, that it seems a bit more readable and browser support is there: https://caniuse.com/mdn-css_types_color_rgb_alpha_parameter
    * removed useless extra clutter in the scss files, which stylint wants
    * whitespace changes for js files
    * removed .scss from scss imports for consistency
    * Changed aditional var to const/let
    * Removed curly string and console warnings, as they are more noise than signal
    * fixed non scss compliant use of rgb
    added stylelint-scss to the plugins
    * changes to make scss files compliant:
    - modules should be used instead of raw global functions
    - `border: 0;` is more constinent to `border: none;`
    - Only let stylint run over css files (it has some isues with the vue syntax)
    * removed the unused function GETRequest()
    * history=>window.history
    * Used Object.values instead of for..in
    Reason:
    for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array
    * added globals for the common js globals
    * allowed dangling underscored and made more things contain underscores
    * fixed how for loops are used:
    .forEach is superiour to for..of due to Polyfills and other issues
    * moved addEventListener to document.addEventListener
    * prefered explicit function-arg defaults, instead of exchanging after the fact
    * moved regenerator-runtime to npm
    * removed CustomEvent polyfill (for IE)
    * mostly unified the functions (named,unnamed, arrow) used by JS
    gulpfile.js is the odd one out.
    * migrated from node-sass to dartSass.
    Reasoning: https://github.com/sass/node-sass/issues/2886
    * reordered the dependencies in the package.json:
    - only things that modern browsers use are getting delivered are listed as dependencys.
    - polyfills/build tooling is listed as below the other Dependencies, because while included in prod, they are only delivered to a minority of users
    * replaces navigatum.cloneState with the buildin structuredClone
    see https://caniuse.com/mdn-api_structuredclone with a polyfill for samsung internet
    * added conditional feedback.js minification
    * changed the method how we rename files in the gulpfile
    * added better maintianed version of spectre.css
    
    Co-authored-by: octycs <octycs@users.noreply.github.com>
    Co-authored-by: octycs <git@octycs.eu>

--- webclient/src/history-states.js
+++ webclient/src/history-states.js
@@ -8,104 +8,104 @@
 
 // TODO: only if initialized and fallback==false
 
-// CustomEvent polyfill (for IE)
-/*(function () {
-  if ( typeof window.CustomEvent === "function" ) return false;
-  function CustomEvent ( event, params ) {
-    params = params || { bubbles: false, cancelable: false, detail: null };
-    var evt = document.createEvent( 'CustomEvent' );
-    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-    return evt;
-   }
-  window.CustomEvent = CustomEvent;
-})();*/
+(() => {
+  const stateIndexSymbol = "__state__index__";
+  const stateDataSymbol = "viewState";
 
-(function(){
-    let stateIndexSymbol = "__state__index__";
-    let stateDataSymbol = "viewState";
-    
-    // If the page is reloaded, the state is preserved by the browser, but we have lost
-    // the state list. For this reason we need to create a dummy one for now.
-    // When navigating to that page again, we may be able to get the old state back (TODO).
-    history.states = [];
-    if (history.state && history.state[stateIndexSymbol] !== undefined) {
-        history.stateIndex = history.state[stateIndexSymbol];
-        for (var i = 0; i < history.stateIndex; i++) {
-            var state = {};
-            state[stateIndexSymbol] = i;
-            history.states.push([state, "", null]);
-        }
-        history.states.push([history.state, "", null]);
+  // If the page is reloaded, the state is preserved by the browser, but we have lost
+  // the state list. For this reason we need to create a dummy one for now.
+  // When navigating to that page again, we may be able to get the old state back (TODO).
+  window.history.states = [];
+  if (
+    window.history.state &&
+    window.history.state[stateIndexSymbol] !== undefined
+  ) {
+    window.history.stateIndex = window.history.state[stateIndexSymbol];
+    for (let i = 0; i < window.history.stateIndex; i++) {
+      const state = {};
+      state[stateIndexSymbol] = i;
+      window.history.states.push([state, "", null]);
+    }
+    window.history.states.push([window.history.state, "", null]);
+  } else {
+    window.history.stateIndex = 0;
+
+    if (window.history.state) {
+      window.history.states.push([window.history.state, "", null]);
     } else {
-        history.stateIndex = 0;
-        
-        if (history.state) {
-            history.states.push([history.state, "", null]);
-        } else {
-            var state = {};
-            state[stateIndexSymbol] = 0;
-            history.states.push([state, "", null]);
-        }
+      const state = {};
+      state[stateIndexSymbol] = 0;
+      window.history.states.push([state, "", null]);
     }
-    
-    history.lastStateIndex = null;
-    let pushState = history.pushState;
-    function add(data,title,url){
-        if(data==null) data={};
-        if(typeof data!="object") data={data:data};
-        data[stateIndexSymbol] = (history.stateIndex+1);
-        history.states.splice(history.stateIndex+1,0,[data,title,url])
-        history.states.splice(history.stateIndex+2)
-        history.stateIndex++;
+  }
+
+  window.history.lastStateIndex = null;
+  const historyPushState = window.history.pushState;
+  function add(data, title, url) {
+    if (data == null) data = {};
+    if (typeof data !== "object") data = { data: data };
+    data[stateIndexSymbol] = window.history.stateIndex + 1;
+    const addedState = [data, title, url];
+    window.history.states.splice(window.history.stateIndex + 1, 0, addedState);
+    window.history.states.splice(window.history.stateIndex + 2);
+    window.history.stateIndex += 1;
+  }
+  window.history.saveCurrentViewState = () => {
+    if (
+      navigatum.router &&
+      navigatum.router.currentRoute.matched[0] &&
+      navigatum.router.currentRoute.matched[0].instances.default.state
+    ) {
+      const stateIndex =
+        window.history.lastStateIndex === null
+          ? window.history.stateIndex
+          : window.history.lastStateIndex;
+
+      window.history.states[stateIndex][0][stateDataSymbol] = structuredClone(
+        navigatum.router.currentRoute.matched[0].instances.default.state
+      );
     }
-    history.saveCurrentViewState = function() {
-        if (navigatum.router
-            && navigatum.router.currentRoute.matched[0]
-            && navigatum.router.currentRoute.matched[0].instances.default.state) {
-            
-            var state_index = history.lastStateIndex === null
-                              ? history.stateIndex : history.lastStateIndex;
-        
-            var cloned = navigatum.cloneState(
-                navigatum.router.currentRoute.matched[0].instances.default.state
-            );
-            history.states[state_index][0][stateDataSymbol] = cloned;
-        }
+  };
+  window.history.pushState = (data, title, url = null) => {
+    add(data, title, url);
+    historyPushState.bind(window.history)(data, title, url);
+  };
+  document.addEventListener("popstate", (e) => {
+    // If navigation is window.history navigation (click on back/forward),
+    // the 'popstate' event is emitted before 'beforeResolve()'.
+    // So in this case, we need to temporarily store the old state index,
+    // so that the saveCurrentViewState() call in beforeResolve() saves the
+    // state to the correct state in the window.history.
+    window.history.lastStateIndex = window.history.stateIndex;
+
+    const eventObject = {};
+    const newStateIndex =
+      e.state != null && e.state[stateIndexSymbol] !== undefined
+        ? e.state[stateIndexSymbol]
+        : 0;
+    eventObject.from = window.history.states[window.history.stateIndex];
+    eventObject.to =
+      newStateIndex in window.history.states
+        ? window.history.states[newStateIndex]
+        : null;
+    eventObject.side =
+      window.history.stateIndex > newStateIndex ? "back" : "forward";
+    // This happens if there is a state in the window.history, that we have lost.
+    // (usually happens on forward navigation from another page)
+    if (!(newStateIndex in window.history.states)) {
+      add(window.history.state, "", window.location.href);
     }
-    history.pushState =function(data,title,url=null){
-        add(data,title,url);
-        pushState.bind(history)(data,title,url);
-    }
-    addEventListener("popstate",function(e){
-        // If navigation is history navigation (click on back/forward),
-        // the 'popstate' event is emitted before 'beforeResolve()'.
-        // So in this case, we need to temporarily store the old state index,
-        // so that the saveCurrentViewState() call in beforeResolve() saves the
-        // state to the correct state in the history.
-        history.lastStateIndex = history.stateIndex;
-        
-        var eventObject= {};
-        var newStateIndex =  e.state != null && e.state[stateIndexSymbol] !== undefined
-                             ? e.state[stateIndexSymbol] : 0;
-        eventObject.from = history.states[history.stateIndex];
-        eventObject.to   = newStateIndex in history.states
-                           ? history.states[newStateIndex] : null;
-        eventObject.side = history.stateIndex > newStateIndex ? "back" : "forward"; 
-        // This happens if there is a state in the history, that we have lost.
-        // (usually happens on forward navigation from another page)
-        if( !(newStateIndex in history.states) ){
-            add(history.state, "", window.location.href);
-        }
-        //window.dispatchEvent(new CustomEvent("historyChange", {detail: eventObject} ))
-        history.stateIndex = e.state!=null && e.state[stateIndexSymbol] !== undefined
-                             ? e.state[stateIndexSymbol] : 0;//-1;
-    });
+    // window.dispatchEvent(new CustomEvent("historyChange", {detail: eventObject} ))
+    window.history.stateIndex =
+      e.state != null && e.state[stateIndexSymbol] !== undefined
+        ? e.state[stateIndexSymbol]
+        : 0; // -1;
+  });
 })();
 
-/*addEventListener("historyChange",function(e){
-    var from = e.detail.from; // [ data , title , url ]
-    var to   = e.detail.to;   // [ data , title , url ]
-    var side = e.detail.side; // "back" | "forward"
+/* addEventListener("historyChange",function(e){
+    const from = e.detail.from; // [ data , title , url ]
+    const to   = e.detail.to;   // [ data , title , url ]
+    const side = e.detail.side; // "back" | "forward"
     console.log(e);
-})*/
-
+}) */
