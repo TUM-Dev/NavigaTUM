226140cc8e0a75c193dba25853027990176c7697
Frank Elsinga <frank@elsinga.de>
2022-07-31 20:00:10 +0200

    Js scss reformttings (#167)
    * Reformatted .inc and our packackage.json to have consistent 2 spaces of padding
    * changed all var's to const/let
    * removed extended data as unused
    * removed leading zeros from scss files.
    This is more consistent with the style used across most of the project
    * removed object shorthands.
    This way is more consistent across the existing codebase
    * switched from the rgba to the rgb css widget.
    Reasoning is, that it seems a bit more readable and browser support is there: https://caniuse.com/mdn-css_types_color_rgb_alpha_parameter
    * removed useless extra clutter in the scss files, which stylint wants
    * whitespace changes for js files
    * removed .scss from scss imports for consistency
    * Changed aditional var to const/let
    * Removed curly string and console warnings, as they are more noise than signal
    * fixed non scss compliant use of rgb
    added stylelint-scss to the plugins
    * changes to make scss files compliant:
    - modules should be used instead of raw global functions
    - `border: 0;` is more constinent to `border: none;`
    - Only let stylint run over css files (it has some isues with the vue syntax)
    * removed the unused function GETRequest()
    * history=>window.history
    * Used Object.values instead of for..in
    Reason:
    for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array
    * added globals for the common js globals
    * allowed dangling underscored and made more things contain underscores
    * fixed how for loops are used:
    .forEach is superiour to for..of due to Polyfills and other issues
    * moved addEventListener to document.addEventListener
    * prefered explicit function-arg defaults, instead of exchanging after the fact
    * moved regenerator-runtime to npm
    * removed CustomEvent polyfill (for IE)
    * mostly unified the functions (named,unnamed, arrow) used by JS
    gulpfile.js is the odd one out.
    * migrated from node-sass to dartSass.
    Reasoning: https://github.com/sass/node-sass/issues/2886
    * reordered the dependencies in the package.json:
    - only things that modern browsers use are getting delivered are listed as dependencys.
    - polyfills/build tooling is listed as below the other Dependencies, because while included in prod, they are only delivered to a minority of users
    * replaces navigatum.cloneState with the buildin structuredClone
    see https://caniuse.com/mdn-api_structuredclone with a polyfill for samsung internet
    * added conditional feedback.js minification
    * changed the method how we rename files in the gulpfile
    * added better maintianed version of spectre.css
    
    Co-authored-by: octycs <octycs@users.noreply.github.com>
    Co-authored-by: octycs <git@octycs.eu>

--- webclient/config.js
+++ webclient/config.js
@@ -1,18 +1,18 @@
 export const configRelease = {
-    /* --- Site configuration --- */
-    // Prefix for resource loading, e.g. "/app/" if the page is
-    // running at "example.com/app/".
-    // Setting it to "" makes paths relative. This only works for
-    // hash-based navigation in development builds.
-    app_prefix: "/",
-    // Prefix for 'cdn' content, e.g. images.
-    cdn_prefix: "https://nav.tum.sexy/cdn/",
-    // Prefix for API requests
-    api_prefix: "https://nav.tum.sexy/api/",
+  /* --- Site configuration --- */
+  // Prefix for resource loading, e.g. "/app/" if the page is
+  // running at "example.com/app/".
+  // Setting it to "" makes paths relative. This only works for
+  // hash-based navigation in development builds.
+  app_prefix: "/",
+  // Prefix for 'cdn' content, e.g. images.
+  cdn_prefix: "https://nav.tum.sexy/cdn/",
+  // Prefix for API requests
+  api_prefix: "https://nav.tum.sexy/api/",
 };
 
 export const configLocal = {
-    app_prefix: "",
-    cdn_prefix: "/cdn/",
-    api_prefix: "http://localhost:8080/api/",
+  app_prefix: "",
+  cdn_prefix: "/cdn/",
+  api_prefix: "http://localhost:8080/api/",
 };
--- webclient/src/legacy.js
+++ webclient/src/legacy.js
@@ -1,11 +1,10 @@
 import "regenerator-runtime/runtime";
 
+/* eslint no-extend-native: "off" */
 // For some reason this polyfill is not included automatically
-if (typeof String.prototype.startsWith === 'undefined') {
-  String.prototype.startsWith = function (needle) {
-    return this.indexOf(needle) === 0;
-  };
+if (typeof String.prototype.startsWith === "undefined") {
+  String.prototype.startsWith = (needle) => this.indexOf(needle) === 0;
 }
 
-/*split*/
+/* split */
 // This comment is here to separate these polyfills and the ones provided by babel from the rest of the code concatenated to this file to make babel work
