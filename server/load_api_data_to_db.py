5865b75fcd668b2e88afb10584cba3d25ae13329
Frank Elsinga <frank@elsinga.de>
2022-08-22 00:46:57 +0200

    added locale handling for /get/:id to the backend

--- server/load_api_data_to_db.py
+++ server/load_api_data_to_db.py
@@ -1,22 +1,51 @@
 import json
 import sqlite3
+from typing import Any, Union
 
-con: sqlite3.Connection = sqlite3.connect("data/api_data.db")
-con.execute(
-    """
-CREATE TABLE IF NOT EXISTS api_data (
-    key     VARCHAR(30) UNIQUE PRIMARY KEY NOT NULL,
-    value   BLOB NOT NULL
-);""",
-)
-# we are using this file in docker, so we don't want to use an acid compliant database ;)
-con.execute("""PRAGMA journal_mode = OFF;""")
-con.execute("""PRAGMA synchronous = OFF;""")
 
-with open("data/api_data.json", encoding="utf-8") as file:
-    data = json.load(file)
-    new_data: list[tuple[str, str]] = [(key, json.dumps(value)) for key, value in data.items()]
+def add_to_database(new_data):
+    """add data consisting of (key, de, en) to the sqlite database"""
+    con: sqlite3.Connection = sqlite3.connect("data/api_data.db")
+    con.execute(
+        """
+    CREATE TABLE IF NOT EXISTS api_data (
+        key     VARCHAR(30) UNIQUE PRIMARY KEY NOT NULL,
+        de      BLOB NOT NULL,
+        en      BLOB NOT NULL
+    );""",
+    )
+    # we are using this file in docker, so we don't want to use an acid compliant database ;)
+    con.execute("""PRAGMA journal_mode = OFF;""")
+    con.execute("""PRAGMA synchronous = OFF;""")
 
-with con:
-    con.executemany("INSERT INTO api_data(key, value) VALUES (?,?)", new_data)
-print("Initialized KV store")
+    with con:
+        con.executemany("INSERT INTO api_data(key, de, en) VALUES (?,?,?)", new_data)
+
+
+def localise(value: Union[str, list[Any], dict[str, Any]], language: str) -> Any:
+    """Recursively localise a dictionary"""
+    if isinstance(value, (bool, float, int, str)) or value is None:
+        return value
+    if isinstance(value, list):
+        return [localise(v, language) for v in value]
+    if isinstance(value, dict):
+        # since we only localise strings, the default to the empty string is safe
+        if "en" in value or "de" in value:
+            return value.get(language, "")
+
+        return {k: localise(v, language) for k, v in value.items()}
+    raise ValueError(f"Unhandled type {type(value)}")
+
+
+def get_localised_data() -> list[tuple[str, str, str]]:
+    """get all data from the json dump and convert it to a list of tuples"""
+    with open("data/api_data.json", encoding="utf-8") as file:
+        data = json.load(file)
+    split_data = [(key, localise(value, "de"), localise(value, "en")) for key, value in data.items()]
+    return [(key, json.dumps(de), json.dumps(en)) for key, de, en in split_data]
+
+
+if __name__ == "__main__":
+    localised_data = get_localised_data()
+    add_to_database(localised_data)
+    print("Initialized KV store")
--- server/src/core/get.rs
+++ server/src/core/get.rs
@@ -1,21 +1,37 @@
-use actix_web::{get, web, HttpResponse};
+use actix_web::{get, web, HttpRequest, HttpResponse};
+use log::error;
 use rusqlite::{Connection, OpenFlags};
 
 #[get("/get/{id}")]
-pub async fn get_handler(params: web::Path<String>) -> HttpResponse {
+pub async fn get_handler(params: web::Path<String>, req: HttpRequest) -> HttpResponse {
     let id = params.into_inner();
     let conn = Connection::open_with_flags(
         "data/api_data.db",
         OpenFlags::SQLITE_OPEN_READ_ONLY | OpenFlags::SQLITE_OPEN_NO_MUTEX,
     )
     .expect("Cannot open database");
-    let mut stmt = conn
-        .prepare_cached("SELECT value FROM api_data WHERE key = ?")
-        .expect("Cannot prepare statement");
-    let result = stmt.query_row([id], |row| {
-        let data: String = row.get_unwrap(0);
-        Ok(data)
-    });
+
+    let en_stmt = conn.prepare_cached("SELECT en FROM api_data WHERE key = ?");
+    let de_stmt = conn.prepare_cached("SELECT de FROM api_data WHERE key = ?");
+    let stmt = match req.cookie("lang") {
+        Some(cookie) => match cookie.value() {
+            "en" => en_stmt,
+            _ => de_stmt,
+        },
+        None => de_stmt,
+    };
+    let result = match stmt {
+        Ok(mut stmt) => stmt.query_row([id], |row| {
+            let data: String = row.get_unwrap(0);
+            Ok(data)
+        }),
+        Err(e) => {
+            error!("Error preparing statement: {:?}", e);
+            return HttpResponse::InternalServerError()
+                .content_type("text/plain")
+                .body("Internal Server Error");
+        }
+    };
     match result {
         Ok(data) => HttpResponse::Ok()
             .content_type("application/json")
