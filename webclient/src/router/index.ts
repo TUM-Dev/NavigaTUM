3acad7eca7e96c88a446a80159d3cd3f9308f9ee
Frank Elsinga <frank@elsinga.de>
2022-08-23 00:20:11 +0200

    added our old routing config

--- webclient/src/router/index.ts
+++ webclient/src/router/index.ts
@@ -1,22 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import MainView from "../views/MainView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
+import SearchView from "../views/SearchView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
+    {path: "/", name: "main", component: MainView,},
+  {
+    path: "/(view|campus|site|building|room)/:id",
+    name: "detail",
+    component: ()=> import("../views/DetailsView.vue"),
+  },
+  { path: "/search",
+    name: "search",
+    component: SearchView },
+  { path: "/api",
+    name: "api",
+    component: ()=> import("../views/APIView.vue"), },
+  { path: "/about/:name",
+    name: "about",
+    component: ()=> import("../views/AboutView.vue"), },
+  { path: "/:catchAll(.*)", component: NotFoundView},
   ],
 });
 
