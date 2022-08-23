1e1c3251e21e471aeeb2a068459365fa3ba66211
Frank Elsinga <frank@elsinga.de>
2022-08-23 17:17:25 +0200

    migrated the apiview to vue3

--- webclient/src/views/APIView.vue
+++ webclient/src/views/APIView.vue
@@ -1,31 +1,18 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import { SwaggerUIBundle } from "swagger-ui-dist"
+import "swagger-ui-dist/swagger-ui.css"
 
-const head = document.getElementsByTagName("head")[0];
-  // Add CSS first (required by swagger-ui)
-  const elCSS = document.createElement("link");
-  elCSS.rel = "stylesheet";
-  elCSS.href = "css/swagger-ui.min.css";
-  head.appendChild(elCSS);
-
-  // JS should trigger init on load
-  const elJS = document.createElement("script");
-  elJS.src = "js/swagger-ui.min.js";
-  elJS.onload = () => {
-    window.setTimeout(() => {
-      // we need to make sure, that swagger-ui exists, otherwise the following command will fail
-      // therefore waiting is effective
-      /* global SwaggerUIBundle */
-      SwaggerUIBundle({
-        url: "https://raw.githubusercontent.com/TUM-Dev/navigatum/main/openapi.yaml",
-        dom_id: "#swagger-ui",
-        presets: [
-          SwaggerUIBundle.presets.apis,
-        ],
-      });
-    }, 10);
-  };
-  head.appendChild(elJS);
+window.setTimeout(() => {
+  // we need to make sure, that swagger-ui exists, otherwise the following command will fail
+  // therefore waiting is effective
+  SwaggerUIBundle({
+    url: "https://raw.githubusercontent.com/TUM-Dev/navigatum/main/openapi.yaml",
+    dom_id: "#swagger-ui",
+    presets: [
+      SwaggerUIBundle.presets.apis,
+    ],
+  });
+}, 10);
 </script>
 
 <template>
