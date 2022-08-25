3a6fe9eb40477aad94e713f8ff48f541a0fdde4f
Frank Elsinga <frank@elsinga.de>
2022-08-25 15:24:19 +0200

    added the ability to pass lang as a query parameter

--- server/src/core/get.rs
+++ server/src/core/get.rs
@@ -1,9 +1,19 @@
 use actix_web::{get, web, HttpRequest, HttpResponse};
 use log::error;
 use rusqlite::{Connection, OpenFlags};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct DetailsQuerryArgs {
+    lang: Option<String>,
+}
 
 #[get("/get/{id}")]
-pub async fn get_handler(params: web::Path<String>, req: HttpRequest) -> HttpResponse {
+pub async fn get_handler(
+    params: web::Path<String>,
+    web::Query(args): web::Query<DetailsQuerryArgs>,
+    req: HttpRequest,
+) -> HttpResponse {
     let id = params.into_inner();
     let conn = Connection::open_with_flags(
         "data/api_data.db",
@@ -13,12 +23,12 @@ pub async fn get_handler(params: web::Path<String>, req: HttpRequest) -> HttpRes
 
     let en_stmt = conn.prepare_cached("SELECT en FROM api_data WHERE key = ?");
     let de_stmt = conn.prepare_cached("SELECT de FROM api_data WHERE key = ?");
-    let stmt = match req.cookie("lang") {
-        Some(cookie) => match cookie.value() {
-            "en" => en_stmt,
-            _ => de_stmt,
-        },
-        None => de_stmt,
+    // we calculate the language from the request by checking if either the query or the cookie are set to en
+    let cookie_en = req.cookie("lang").map_or(false, |c| c.value() == "en");
+    let arg_en = args.lang.map_or(false, |c| c == "en");
+    let stmt = match arg_en || cookie_en {
+        true => en_stmt,
+        false => de_stmt,
     };
     let result = match stmt {
         Ok(mut stmt) => stmt.query_row([id], |row| {
