7b88b7e0310658314c95d7109e4d5688c17b2c9d
Frank Elsinga <frank@elsinga.de>
2022-08-25 14:00:49 +0200

    fixed origin check to allow local-remote development

--- webclient/src/utils/fetch.ts
+++ webclient/src/utils/fetch.ts
@@ -8,7 +8,12 @@ export function useFetch<T>(
   const data = ref<T | null>(null);
   const error = ref<string | null>(null);
 
-  options.credentials = import.meta.env.PROD ? "same-origin" : "include";
+  // we can only add the lang cookie if
+  // - the url is local and thus accepting certificates or
+  // - we are in production, and thus we are the same origin
+  if (import.meta.env.PROD) options.credentials = "same-origin";
+  else if (import.meta.env.API_URL?.startsWith("http://localhost"))
+    options.credentials = "include";
 
   fetch(url, options)
     .then((res) => res.json())
