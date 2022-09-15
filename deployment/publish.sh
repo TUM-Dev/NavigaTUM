065aa7f73c1cf9ce24c87acea5c4aa91f2f5da74
Frank Elsinga <frank@elsinga.de>
2022-09-15 20:53:02 +0200

    allowed deploying updates to already existing deployments

--- deployment/publish.sh
+++ deployment/publish.sh
@@ -9,35 +9,35 @@ tmp_dir=$(mktemp -d -t navigatum-XXXXXXXXXX)
 #trap "trap - SIGTERM && rm -fr $tmp_dir" SIGINT SIGTERM EXIT
 echo "Temporary files will be written to: $tmp_dir"
 
-
-(
-cd ./deployment || exit
 if [ $1 == "main" ]; then
     echo "Deploying to production"
-    rm k3s/templates/application.yaml
+    export TARGET=main
 
-    mkdir "$tmp_dir/main"
-    cp -R k3s/* "$tmp_dir/main"
+    rm ./deployment/k3s/templates/application.yaml
+
+    mkdir "$tmp_dir/$TARGET"
+    cp -R ./deployment/k3s/* "$tmp_dir/main"
 else
     echo "Deploying to PR-based environment"
 
     export BRANCH=$1
     export PR_NUMBER=$2
-    echo "creating app for $BRANCH on pr-$PR_NUMBER"
+    export TARGET=pr-$PR_NUMBER
+    echo "creating app for $BRANCH on $TARGET"
 
     # In PR-based environments, someone knowing the Meili master key is not a concern
     MEILI_MASTER_KEY="$(cat /dev/urandom | tr -dc '[:alpha:]' | head -c 64|base64)"
     export MEILI_MASTER_KEY
 
-    mkdir "$tmp_dir/pr-$PR_NUMBER"
-    cat k3s/templates/application.yaml | kyml tmpl --env BRANCH --env PR_NUMBER --env MEILI_MASTER_KEY > tmp.yaml
-    cp -R k3s/* "$tmp_dir/pr-$PR_NUMBER"
-    mv tmp.yaml "$tmp_dir/pr-$PR_NUMBER/templates/application.yaml"
+    mkdir "$tmp_dir/$TARGET"
+    cat ./deployment/k3s/templates/application.yaml | kyml tmpl --env BRANCH --env PR_NUMBER --env MEILI_MASTER_KEY > tmp.yaml
+    cp -R ./deployment/k3s/* "$tmp_dir/$TARGET"
+    mv tmp.yaml "$tmp_dir/$TARGET/templates/application.yaml"
 fi
-)
 
 echo "checking out deployment branch"
 git checkout deployment --
+rm -fr "./$TARGET"
 mv $tmp_dir/* .
 git add .
 
