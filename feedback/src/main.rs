d7626082b2665fbc0617bab50afc246678436154
Frank Elsinga <frank@elsinga.de>
2022-12-22 21:22:53 +0100

    Feedback microservice (#325)
    * migrated the feedback service into an own subservice
    * added an additional deployment
    * added a CD for this service
    * adapted our docs

--- feedback/src/main.rs
+++ feedback/src/main.rs
@@ -1,13 +1,25 @@
 use actix_cors::Cors;
 use actix_web::{get, middleware, web, App, HttpResponse, HttpServer};
 
+use structopt::StructOpt;
+
 mod core;
-mod maps;
-mod utils;
 
 const MAX_JSON_PAYLOAD: usize = 1024 * 1024; // 1 MB
 
-#[get("/api/source_code")]
+#[derive(StructOpt, Debug)]
+#[structopt(name = "server")]
+pub struct Opt {
+    // Feedback
+    /// GitHub personal access token
+    #[structopt(short = "t", long)]
+    github_token: Option<String>,
+    /// Secret for the feedback token generation
+    #[structopt(short = "jwt", long)]
+    jwt_key: Option<String>,
+}
+
+#[get("/api/feedback/source_code")]
 async fn source_code_handler() -> HttpResponse {
     let gh_base = "https://github.com/TUM-Dev/navigatum".to_string();
     let commit_hash = std::env::var("GIT_COMMIT_SHA");
@@ -20,7 +32,7 @@ async fn source_code_handler() -> HttpResponse {
         .body(github_link)
 }
 
-#[get("/api/health")]
+#[get("/api/feedback/health")]
 async fn health_handler() -> HttpResponse {
     HttpResponse::Ok()
         .content_type("text/plain")
@@ -31,24 +43,36 @@ async fn health_handler() -> HttpResponse {
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
+    let mut opt = Opt::from_args();
+    if opt.github_token.is_none() {
+        opt.github_token = std::env::var("GITHUB_TOKEN").ok();
+    }
+    if opt.jwt_key.is_none() {
+        opt.jwt_key = std::env::var("JWT_KEY").ok();
+    }
+
+    let state_feedback = web::Data::new(core::init_state(opt));
     HttpServer::new(move || {
         let cors = Cors::default()
             .allow_any_origin()
             .allow_any_header()
-            .allowed_methods(vec!["GET"])
+            .allowed_methods(vec!["GET", "POST"])
             .max_age(3600);
 
         App::new()
             .wrap(cors)
-            .wrap(middleware::Logger::default().exclude("/api/health"))
+            .wrap(middleware::Logger::default().exclude("/api/feedback/health"))
             .wrap(middleware::Compress::default())
             .app_data(web::JsonConfig::default().limit(MAX_JSON_PAYLOAD))
             .service(source_code_handler)
             .service(health_handler)
-            .service(web::scope("/api/preview").configure(maps::configure))
-            .service(web::scope("/api").configure(core::configure))
+            .service(
+                web::scope("/api/feedback")
+                    .configure(core::configure)
+                    .app_data(state_feedback.clone()),
+            )
     })
-    .bind(std::env::var("BIND_ADDRESS").unwrap_or_else(|_| "0.0.0.0:8080".to_string()))?
+    .bind(std::env::var("BIND_ADDRESS").unwrap_or_else(|_| "0.0.0.0:6001".to_string()))?
     .run()
     .await
 }
