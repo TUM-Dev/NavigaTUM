40dce1f8964d841b37ff09817909b79bce0b3358
Frank Elsinga <frank@elsinga.de>
2023-02-06 01:49:39 +0100

    fixed two paths in the main api being wrong

--- server/main-api/src/core/mod.rs
+++ server/main-api/src/core/mod.rs
@@ -2,12 +2,10 @@ use actix_web::web;
 
 mod get;
 mod legacy_redirect;
-mod list;
 mod search;
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(get::get_handler);
     cfg.service(legacy_redirect::legacy_redirect_handler);
     cfg.service(search::search_handler);
-    cfg.service(list::ids_with_calendar);
 }
