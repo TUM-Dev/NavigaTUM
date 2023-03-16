d36a7ea03f0583439cb50b32bb5b0b2e8b3d8cdf
Frank Elsinga <frank@elsinga.de>
2023-03-16 18:45:09 +0100

    Statefull calendar (#423)
    * ripped out the previous dev-prod partitioning scheme and replaced it with `last_scrape`
    
    * chand the calendar deployment to a statefull set
    
    * renamed `duration` -> `time_window` by code review

--- server/calendar/src/schema.rs
+++ server/calendar/src/schema.rs
@@ -22,32 +22,6 @@ diesel::table! {
         status_id -> Text,
         status -> Text,
         comment -> Text,
+        last_scrape -> Timestamp,
     }
 }
-
-diesel::table! {
-    calendar_scrape (single_event_id) {
-        key -> Text,
-        dtstart -> Timestamp,
-        dtend -> Timestamp,
-        dtstamp -> Timestamp,
-        event_id -> Integer,
-        event_title -> Text,
-        single_event_id -> Integer,
-        single_event_type_id -> Text,
-        single_event_type_name -> Text,
-        event_type_id -> Text,
-        event_type_name -> Nullable<Text>,
-        course_type_name -> Nullable<Text>,
-        course_type -> Nullable<Text>,
-        course_code -> Nullable<Text>,
-        course_semester_hours -> Nullable<Integer>,
-        group_id -> Nullable<Text>,
-        xgroup -> Nullable<Text>,
-        status_id -> Text,
-        status -> Text,
-        comment -> Text,
-    }
-}
-
-diesel::allow_tables_to_appear_in_same_query!(calendar, calendar_scrape);
