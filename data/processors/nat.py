2261efdbef9ac24f7971ecd02f2bae5820e5026f
octycs <git@octycs.eu>
2023-04-01 10:38:15 +0200

    Run linter

--- data/processors/nat.py
+++ data/processors/nat.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 
 @dataclass
 class NATBuilding:
-    b_id: str
+    b_id: None | str
     b_code: str
     b_name: str
     b_tumonline_id: None | int
@@ -22,6 +22,7 @@ class NATBuilding:
         self.b_address = data["address"]
 
     def as_dict(self):
+        """Return the building data as dict"""
         return self.__dict__
 
 
@@ -41,73 +42,96 @@ def merge_nat_buildings(data):
 
     for building in [NATBuilding(b) for b in buildings]:
         if building.b_code in [
-            "0000",  # 'Building' 0000 contains some buildings and places not in TUMonline as rooms.
+            # 'Building' 0000 contains some buildings and places not in TUMonline as rooms
+            "0000",
             # Teared down and replaced by 2330-2334 most likely
-            "2301", "2302", "2303", "2304", "2305", "2306", "2307", "2308",
+            "2301",
+            "2302",
+            "2303",
+            "2304",
+            "2305",
+            "2306",
+            "2307",
+            "2308",
             # Unclear, but probably no longer existing or in use by TUM
-            "2360", # Pfortencontainer C1
-            "2361", # Umkleide-Werkstatt-Container C2
-            "2904", # Nymphenburgerstr. 39
-
+            "2360",  # Pfortencontainer C1
+            "2361",  # Umkleide-Werkstatt-Container C2
+            "2904",  # Nymphenburgerstr. 39
             # "Institut für Grünlandlehre u. Haushaltstechnik"
             # Either no longer existing or it was renamed.
             "4201",
-
             # The following buildings should be in Dürnast from their ID, but according
             # to TUMonline they don't have any rooms or don't exist. It's hard to find
             # out where they are from online sources.
-            "4504", # STG.WST.-J. Viehhofstall
-            "4507", # STG.WST.-Trafo, Pumpstation
-            "4511", # STG.WST.-Biogasanlage
-            "4525", # STG.WST.-Mobiler Hühnerstall
-
+            "4504",  # STG.WST.-J. Viehhofstall
+            "4507",  # STG.WST.-Trafo, Pumpstation
+            "4511",  # STG.WST.-Biogasanlage
+            "4525",  # STG.WST.-Mobiler Hühnerstall
             # Hirschau was sold to the Munich Airport by TUM:
             # https://www.merkur.de/lokales/erding/versuchsgut-hirschau-verkauft-247253.html
-            "4701", "4702", "4703", "4704", "4705", "4706", "4707", "4708", "4709", "4710",
-            "4711", "4712", "4713", "4714", "4715", "4716", "4717", "4718", "4719", "4720",
+            "4701",
+            "4702",
+            "4703",
+            "4704",
+            "4705",
+            "4706",
+            "4707",
+            "4708",
+            "4709",
+            "4710",
+            "4711",
+            "4712",
+            "4713",
+            "4714",
+            "4715",
+            "4716",
+            "4717",
+            "4718",
+            "4719",
+            "4720",
             "4721",
-
             # "Arb.Geb. {1,2,3}, SCHP" probably changed to 4914-4916
-            "4911", "4912", "4913",
-
+            "4911",
+            "4912",
+            "4913",
             # "Gästehaus" (5105) and "GRS Neubau" (5106). I can't find where they are.
             # the "Gästehaus" might be teared down already.
-            "5105", "5106",
-
+            "5105",
+            "5106",
             # "RCM-Büro-Containerbau"; Has only rooms in the NAT roomfinder, but all of them
             # empty placeholders without data. Maybe no longer existing.
             "5269",
-
             # Former ITEM (Innovationszentrum für Therapeutische Medizintechnik), but placed
             # exactly where 5701 is now, probably no longer existing.
             # https://portal.mytum.de/pressestelle/pressemitteilungen/news-500)
             "5702",
-
             # Others
             # ¹WZW Source: https://wiki.tum.de/download/attachments/1318389295/141016_WZW_Plan_Final.pdf?api=v2
-            "4118", # "Kindervilla II"; Maybe ID change from 4118 to 4117
-            "4198", # "ehem. Wasserwerk"; Probably ID change from 4198 to 4192
-            "4228", # "Trafostation XI"; Maybe ID change from 4228 to 4281
-            "4305", # "Tierernährung NGB"; Probably now part of 4318¹
-            "4306", # "Tierernährung HGB"; Probably now part of 4318¹
-            "4312", # "Zierpflanzenbau Gewächshaus"; Probably ID change from 4311 to 4379¹
-            "4313", # "Zierpflanzenbau Wirtschaftsgebäude"; Can't find any location
-            "4919", # "Getreidesilo"; Maybe ID change from 4919 to 4010
-
+            "4118",  # "Kindervilla II"; Maybe ID change from 4118 to 4117
+            "4198",  # "ehem. Wasserwerk"; Probably ID change from 4198 to 4192
+            "4228",  # "Trafostation XI"; Maybe ID change from 4228 to 4281
+            "4305",  # "Tierernährung NGB"; Probably now part of 4318¹
+            "4306",  # "Tierernährung HGB"; Probably now part of 4318¹
+            "4312",  # "Zierpflanzenbau Gewächshaus"; Probably ID change from 4311 to 4379¹
+            "4313",  # "Zierpflanzenbau Wirtschaftsgebäude"; Can't find any location
+            "4919",  # "Getreidesilo"; Maybe ID change from 4919 to 4010
             # Location unknown
-            "2603", # Tankstelle
-            "4112", # "Institutsgebäude Brauwesen"; Maybe 48.39508 / 11.72437 (near 4111)
-            "4133", # Wohngebäude Ganzenmüllerstr.
-            "4280", # Ehem. Arbeitsamt
-            "4303", # Vegetationsh. Inst. Pfl. und Züchtung
-            "5216", # CO 60 Quelle
-
+            "2603",  # Tankstelle
+            "4112",  # "Institutsgebäude Brauwesen"; Maybe 48.39508 / 11.72437 (near 4111)
+            "4133",  # Wohngebäude Ganzenmüllerstr.
+            "4280",  # Ehem. Arbeitsamt
+            "4303",  # Vegetationsh. Inst. Pfl. und Züchtung
+            "5216",  # CO 60 Quelle
             # They might be integrated custom somewhere else, but here we ignore these.
             "3002",  # "Testgebäude 2" => building which probably does not exist
             "5110",  # wurde Abgerissen
             "5537",  # "Interims-Zelt-TUMshop" => building which no longer exists
-            "0598", "4298", "5538", "5998",  # "Interims-Tentomax => buildings no longer exist
-            "5516", "5600",  # phantom buildings, which don't exist
+            "0598",
+            "4298",
+            "5538",
+            "5998",  # "Interims-Tentomax => buildings no longer exist
+            "5516",
+            "5600",  # phantom buildings, which don't exist
             "XXXX",  # "virtueller Raum"
         ]:
             continue
@@ -133,8 +157,9 @@ def _infer_internal_id(building, data):
                 raise RuntimeError(f"Building id '{building.b_code}' more than once in base data")
             building.b_id = _id
     if building.b_id is None:
-        raise RuntimeError(f"Building '{building}' not found in base data. "
-                           f"It may be missing in the areatree.")
+        raise RuntimeError(
+            f"Building '{building}' not found in base data. " f"It may be missing in the areatree.",
+        )
     return building.b_id
 
 
