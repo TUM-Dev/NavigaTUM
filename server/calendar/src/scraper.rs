60b1035f6a8b71b515150caaba93119d933795ad
Frank Elsinga <frank@elsinga.de>
2023-04-05 18:59:58 +0200

    migrated from lens internal prometheus to kube-stack prometheus

--- server/calendar/src/scraper.rs
+++ server/calendar/src/scraper.rs
@@ -51,7 +51,7 @@ async fn main() {
     info!("Pushing metrics to the pushgateway");
     tokio::task::spawn_blocking(move || {
         let address = std::env::var("PUSHGATEWAY_ADRESS")
-            .unwrap_or("pushgateway.lens-metrics.svc.cluster.local".to_string());
+            .unwrap_or("pushgateway.monitoring.svc.cluster.local".to_string());
         prometheus::push_metrics(
             "navigatum_calendarscraper",
             labels! {"duration".to_owned() => format!("{time_window:?}"),},
