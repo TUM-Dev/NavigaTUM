0197bd72cabc21c730baf09ced09587d1543fbc0
Frank Elsinga <frank@elsinga.de>
2023-05-15 21:54:08 +0200

    removed the dependency of the roomformatter on the implicit length and indexing

--- server/main-api/src/search/search_executor/formatter.rs
+++ server/main-api/src/search/search_executor/formatter.rs
@@ -1,4 +1,4 @@
-use crate::search::search_executor::parser::ParsedQuery;
+use crate::search::search_executor::parser::{ParsedQuery, TextToken};
 use crate::search::search_executor::query::MSHit;
 use crate::search::search_executor::ResultEntry;
 use unicode_truncate::UnicodeTruncateStr;
@@ -21,70 +21,75 @@ impl RoomVisitor {
     }
     // Parse the search against some known room formats and improve the
     // results display in this case. Room formats are hardcoded for now.
-    pub(super) fn parse_room_formats(&self, hit: &MSHit) -> Option<String> {
-        // Some building specific roomcode formats are determined by their building prefix
-        if self.parsed_input.len() == 2
-            && match self.parsed_input[0].as_str() {
-                "mi" => hit.id.starts_with("560") || hit.id.starts_with("561"),
-                "mw" => hit.id.starts_with("550") || hit.id.starts_with("551"),
-                "ph" => hit.id.starts_with("5101"),
-                "ch" => hit.id.starts_with("540"),
-                _ => false,
+    fn parse_room_formats(&self, hit: &MSHit) -> Option<String> {
+        let first_token = self.parsed_input.tokens.first()?;
+        let archname = hit.arch_name.clone()?;
+        match first_token {
+            TextToken::SplittableText((t0, t1))
+                if self.parsed_input.relevant_enough_for_room_highligting() =>
+            {
+                let building_specific_roomcode_format_determinable_by_building_prefix =
+                    match t0.as_str() {
+                        "mi" => hit.id.starts_with("560") || hit.id.starts_with("561"),
+                        "mw" => hit.id.starts_with("550") || hit.id.starts_with("551"),
+                        "ph" => hit.id.starts_with("5101"),
+                        "ch" => hit.id.starts_with("540"),
+                        _ => false,
+                    };
+                if !building_specific_roomcode_format_determinable_by_building_prefix
+                    || !archname.starts_with(t1)
+                {
+                    return None;
+                }
+                let arch_id = archname.split('@').next()?;
+                let split_arch_id = unicode_split_at(arch_id, t0.chars().count());
+                Some(format!(
+                    "{}{} {}{}{}",
+                    self.highlighting.0,
+                    t0.to_uppercase(),
+                    split_arch_id.0,
+                    self.highlighting.1,
+                    split_arch_id.1,
+                ))
             }
-            && !self.parsed_input[1].contains('@')
-            && hit.arch_name.is_some()
-            && hit
-                .arch_name
-                .as_ref()
-                .unwrap()
-                .starts_with(&self.parsed_input[1])
-        {
-            let arch_id = hit.arch_name.as_ref().unwrap().split('@').next().unwrap();
-            let split_arch_id = unicode_split_at(arch_id, self.parsed_input[1].chars().count());
-            Some(format!(
-                "{}{} {}{}{}",
-                self.highlighting.0,
-                self.parsed_input[0].to_uppercase(),
-                split_arch_id.0,
-                self.highlighting.1,
-                split_arch_id.1,
-            ))
-        // If it doesn't match some precise room format, but the search is clearly
-        // matching the arch name and not the main name, then we highlight this arch name.
-        // This is intentionally still restrictive and considers only the first token,
-        // because we expect searches for arch names not to start with anything else.
-        } else if (self.parsed_input.len() == 1
-             || (self.parsed_input.len() > 1 && self.parsed_input[0].len() >= 3))
-        //     Needs to be specific enough to be considered relevant ↑
-        && !hit.name.contains(&self.parsed_input[0]) // No match in the name
-        && !hit.parent_building_names.is_empty() // Has parent information to show in query
-        && hit.arch_name.is_some()
-        && hit
-            .arch_name
-            .as_ref()
-            .unwrap()
-            .starts_with(&self.parsed_input[0])
-        {
-            let (prefix, parsed_arch_id) = self.split_prefix_from_arch_building_id(&hit);
-            let parsed_aid =
-                unicode_split_at(&parsed_arch_id, self.parsed_input[0].chars().count());
-            Some(format!(
-                "{}{}{}{}{}",
-                prefix.unwrap_or_default(),
-                self.highlighting.0,
-                parsed_aid.0,
-                self.highlighting.1,
-                parsed_aid.1,
-            ))
-        } else {
-            None
+            TextToken::SplittableText(_) => None,
+
+            // If it doesn't match some precise room format, but the search is clearly
+            // matching the arch name and not the main name, then we highlight this arch name.
+            // This is intentionally still restrictive and considers only the first token,
+            // because we expect searches for arch names not to start with anything else.
+            TextToken::Text(text) if self.parsed_input.relevant_enough_for_room_highligting() => {
+                let no_parent_information_to_show_in_query = hit.parent_building_names.is_empty();
+                if hit.name.contains(text)
+                    || no_parent_information_to_show_in_query
+                    || !archname.starts_with(text)
+                {
+                    return None;
+                }
+
+                let (prefix, parsed_arch_id) = self.split_prefix_from_arch_building_id(&hit, text);
+                let parsed_aid = unicode_split_at(&parsed_arch_id, text.chars().count());
+                Some(format!(
+                    "{}{}{}{}{}",
+                    prefix.unwrap_or_default(),
+                    self.highlighting.0,
+                    parsed_aid.0,
+                    self.highlighting.1,
+                    parsed_aid.1,
+                ))
+            }
+            TextToken::Text(_) => None,
         }
     }
 
     /// Exclude the part after the "@" if it's not in the query and use the
     /// building name instead, because this is probably more helpful
-    fn split_prefix_from_arch_building_id<'a>(&self, hit: &&MSHit) -> (Option<&'a str>, String) {
-        if self.parsed_input[0].contains('@') {
+    fn split_prefix_from_arch_building_id<'a>(
+        &self,
+        hit: &&MSHit,
+        first_token: &str,
+    ) -> (Option<&'a str>, String) {
+        if first_token.contains('@') {
             return (None, hit.arch_name.as_ref().unwrap().to_string());
         }
         let arch_id = hit.arch_name.as_ref().unwrap().split('@').next().unwrap();
