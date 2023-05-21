57140b0f827a04cc5d4e802b4a928c1c96834d3d
Frank Elsinga <frank@elsinga.de>
2023-05-21 18:56:28 +0200

    tested a different way to scope cookies

--- webclient/src/composables/cookies.ts
+++ webclient/src/composables/cookies.ts
@@ -1,12 +1,6 @@
-export function saveCooke(name:string, value:string, reload = true) {
+export function saveCooke(name: string, value: string, reload = true) {
   localStorage.setItem(name, value);
-  // in our staging environment, we want to set the cookie too
-  // domain=.. is set on the main site
-  // on staging Path=.. is set to allow for changing cookies
-  let scoping = `Domain=${import.meta.env.VITE_APP_URL}`;
-  if (window.location.host !== import.meta.env.VITE_APP_URL) {
-    scoping = `Path=${window.location.origin}`;
-  }
-  document.cookie = `${name}=${value};Max-Age=31536000;SameSite=Lax;${scoping}`;
+  document.cookie = `${name}=${value};Max-Age=31536000;SameSite=Strict;Path=/`;
+  alert(document.cookie);
   if (reload) window.location.reload();
 }
