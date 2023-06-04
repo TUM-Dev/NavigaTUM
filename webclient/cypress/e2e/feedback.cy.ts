b826410c79ec2743881ea3ce15213584c94434d5
Frank Elsinga <frank@elsinga.de>
2023-06-04 14:40:39 +0200

    fixed the frontend tests not running because of missing labels

--- webclient/cypress/e2e/feedback.cy.ts
+++ webclient/cypress/e2e/feedback.cy.ts
@@ -22,10 +22,10 @@ describe("Check if opening the feedback form works from every subview", () => {
 function checkFeedbackForm(selector_which_should_open_the_modal: string) {
   // open the modal
   cy.intercept("POST", "/api/feedback/get_token", { statusCode: 201, fixture: "feedback_token.json" });
-  cy.get("#feedback-modal").should("not.exist");
+  cy.get('[data-cy="feedback-modal"]').should("not.exist");
   cy.get(selector_which_should_open_the_modal).click({ scrollBehavior: false });
   // check that the modal is opened
-  cy.get("#feedback-modal").should("exist");
+  cy.get('[data-cy="feedback-modal"]').should("exist");
   cy.get("#feedback-error").should("be.empty");
   cy.get("#feedback-privacy").should("not.be.checked");
   cy.get("#feedback-delete").should("not.be.checked");
@@ -48,16 +48,16 @@ function checkFeedbackForm(selector_which_should_open_the_modal: string) {
 
   // accept the privacy policy
   cy.get("#feedback-privacy").parent().click();
-  cy.get("#feedback-modal .modal-body").scrollTo("bottom");
+  cy.get('[data-cy="feedback-modal"] .modal-body').scrollTo("bottom");
   cy.intercept("POST", "/api/feedback/feedback", { statusCode: 201, fixture: "feedback_response.json" });
   cy.get("#feedback-send").click();
 
   // check that the next page is loaded correctly
-  cy.get("#feedback-modal", { timeout: 10_000 }).should("not.exist"); // wait for the site to be interactive
-  cy.get("#feedback-success-modal").should("exist");
+  cy.get('[data-cy="feedback-modal"]', { timeout: 10_000 }).should("not.exist"); // wait for the site to be interactive
+  cy.get('[data-cy="feedback-success-modal"]').should("exist");
   cy.contains("Vielen Dank für dein Feedback!");
   cy.contains("OK").click();
-  cy.get("#feedback-success-modal").should("not.exist");
+  cy.get('[data-cy="feedback-success-modal"]').should("not.exist");
 }
 
 export {};
