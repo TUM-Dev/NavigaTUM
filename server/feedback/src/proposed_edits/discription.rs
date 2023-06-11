f7015db2097e32d5c41e5c9ae324f6d998500a4e
Frank Elsinga <frank@elsinga.de>
2023-06-11 14:03:30 +0200

    [feedback-pipeline-5] Basic implementation of the `/api/feedback/propose_edit` API Endpoint (#508)
    * implemented the basics of an extendible feedback architecture
    
    * bumped the package.json
    
    * downgrade of libc to 0.2.144 due to https://github.com/rust-random/getrandom/issues/363

---
+++ server/feedback/src/proposed_edits/discription.rs
@@ -0,0 +1,92 @@
+use crate::proposed_edits::AppliableEdit;
+use std::collections::HashMap;
+use std::path::Path;
+
+pub struct Description {
+    pub title: String,
+    pub body: String,
+}
+
+impl Default for Description {
+    fn default() -> Self {
+        Self {
+            title: "[empty commit]".to_string(),
+            body: Default::default(),
+        }
+    }
+}
+
+impl Description {
+    pub(crate) fn add_context(&mut self, additional_context: &str) {
+        if !additional_context.is_empty() {
+            self.body += &format!("Additional context: {additional_context}\n");
+        }
+    }
+    pub(crate) fn appply_set<T: AppliableEdit>(
+        &mut self,
+        category_name: &'static str,
+        set: HashMap<String, T>,
+        base_dir: &Path,
+    ) {
+        if !set.is_empty() {
+            self.title += &format!("{amount} {category_name} edits", amount = set.len());
+
+            self.body += &format!("The following {category_name} edits were made:\n");
+
+            self.body += "| entry | edit | \n";
+            self.body += "| --- | --- | \n";
+            for (key, value) in set {
+                let result = value.apply(&key, base_dir);
+                self.body += &format!("| [`{key}`](https://nav.tum.de/view/{key}) | {result} |\n");
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_discription {
+    use crate::proposed_edits::AppliableEdit;
+    use std::collections::HashMap;
+    use std::path::Path;
+
+    use super::*;
+
+    #[test]
+    fn test_description_context() {
+        let mut description = Description {
+            title: "title".to_string(),
+            body: "body\n".to_string(),
+        };
+        description.add_context("context");
+        description.add_context(""); // should be a noop
+        assert_eq!(description.title, "title");
+        assert_eq!(description.body, "body\nAdditional context: context\n");
+    }
+
+    #[derive(Default)]
+    struct TestEdit;
+    impl AppliableEdit for TestEdit {
+        fn apply(&self, _key: &str, _base_dir: &Path) -> String {
+            "applied_value".to_string()
+        }
+    }
+
+    #[test]
+    fn test_apply_set_empty() {
+        let mut description = Description::default();
+        let set: HashMap<String, TestEdit> = HashMap::default();
+        description.appply_set("category", set, Path::new(""));
+        assert_eq!(description.title, "");
+        assert_eq!(description.body, "");
+    }
+
+    #[test]
+    fn test_apply_set() {
+        let mut description = Description::default();
+        let mut set = HashMap::new();
+        set.insert("key".to_string(), TestEdit::default());
+        description.appply_set("category", set, Path::new(""));
+        assert_eq!(description.title, "1 category edits");
+        assert_eq!(description.body, "The following category edits were made:\n| entry | edit | \n| --- | --- | \n| [`key`](https://nav.tum.de/view/key) | applied_value |\n");
+    }
+}
