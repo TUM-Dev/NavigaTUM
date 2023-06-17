763f527f9475a9abf561ac43ed31f234b1251e4a
quarz12 <danielmartinqwe@gmail.com>
2023-06-17 19:13:45 +0200

    removed dataclasses to allow serialization

--- data/external/scrapers/public_transport.py
+++ data/external/scrapers/public_transport.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List
 from external.scraping_utils import cached_json
 from external.scrapers.roomfinder import scrape_maps
 import csv
@@ -22,46 +21,33 @@ def avg(x1, x2):
     return (x1 + x2) / 2
 
 
-@dataclass(frozen=True)
-class Coords:
-    lat: Decimal
-    lon: Decimal
+def nearby(building_coords:tuple, stations: list[dict]) -> list[dict]:
+    results=[]
+    for station in stations:
+        distance_to_building=round(distance((station.get("lat"), station.get("lon")), (building_coords)).km,2)
+        if distance_to_building <= MAXDISTANCE:
+            station["lat"]=str(station.get("lat"))  #decimal to string to allow json serialization
+            station["lon"]=str(station.get("lon"))
+            station["distance"]=distance_to_building
+            results.append(station)#TODO map decimal to string then add to list
+    return results
 
 
-@dataclass
-class Station:
-    id: int
-    Name: str
-    Ort: str
-    global_id: str
-    lat: Decimal
-    lon: Decimal
-
-
-def nearby(building: Coords, stations: List[Station]) -> List[Station]:
-    return list(
-        filter(
-            lambda station: distance((station.lat, station.lon), (building.lat, building.lon)).km <= MAXDISTANCE,
-            stations,
-        )
-    )
-
-
-# @cached_json("public_transport.json")
+@cached_json("public_transport.json")
 def scrape_stations():
     with Path("data/external/scrapers/MVV_HSTReport2212.csv").open("r") as file:
         lines = list(csv.reader(file, delimiter=";"))[1:]  # ignore first line as it contains row names
         lines = filter(lambda l: not all(not bool(i) for i in l), lines)  # filter out lines where each value is ''
     stations = list(
         map(
-            lambda station: Station(
-                station[STATIONID],
-                station[NAME],
-                station[ORT],
-                station[GLOBAL_ID],
-                Decimal(station[WGS84X].replace(",", ".")),
-                Decimal(station[WSG84Y].replace(",", ".")),
-            ),
+            lambda station: {
+                "id":station[STATIONID],
+                "name":station[NAME],
+                "ort":station[ORT],
+                "global-id":station[GLOBAL_ID],
+                "lat":Decimal(station[WGS84X].replace(",", ".")),
+                "lon":Decimal(station[WSG84Y].replace(",", ".")),
+            },
             lines,
         )
     )
@@ -71,14 +57,10 @@ def scrape_stations():
         # ignore entries that dont have assigned lon/lat
         if latlonbox := building.get("latlonbox"):
             # since lat and lon have rather long decimal points it is probably best to not use floats
-            latn, lats = Decimal(latlonbox["north"]), Decimal(latlonbox["south"])
+            latn, lats = Decimal(latlonbox.get("north")), Decimal(latlonbox.get("south"))
             lone, lonw = Decimal(latlonbox["east"]), Decimal(latlonbox["west"])
             lat: Decimal = avg(latn, lats)
             lon: Decimal = avg(lone, lonw)
-            coords = Coords(lat, lon)
-            building_coords.append({"id": building.get("id"), "desc": building.get("desc"), "center-coord": coords})
-            print(nearby(coords, stations))
-            print(building)
-            print(coords)
+            building_coords.append({"id": building.get("id"), "desc": building.get("desc"), "center-coord": {"lat":str(lat),"lon":str(lon)}, "public-transport":nearby((lat,lon),stations)})
 
-    # print(building_coords)
+    return building_coords
