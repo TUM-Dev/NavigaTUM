961ae7f7762d01eec4e16e31abac9da2106f6464
quarz12 <danielmartinqwe@gmail.com>
2023-06-22 15:33:29 +0200

    changed to use a more detailed csv. this one also includes substations, therefore now generates a dict with parent stations and substations associated to them

--- data/external/scrapers/public_transport.py
+++ data/external/scrapers/public_transport.py
@@ -5,40 +5,49 @@ import csv
 from decimal import Decimal
 from processors.public_transport import nearby
 # CSV indexes
-STATIONID = 0
-NAME = 1
-ORT = 2
-GLOBAL_ID = 3
-WGS84X = 4  # lat
-WSG84Y = 6  # lon
+STATIONID = "stop_id"
+NAME = "stop_name"
+TYPE= "location_type"
+WGS84X = "stop_lat"  # lat
+WSG84Y = "stop_lon"  # lon
+PARENT="parent_station"
 
 def avg(x1, x2):
     return (x1 + x2) / 2
 
 @cached_json("public_transport.json")
 def scrape_stations():
-    with Path("scrapers/MVV_HSTReport2212.csv").open("r") as file:
-        lines = csv.reader(file, delimiter=";")  
-        next(lines) # skip first line as it contains row names
-        lines = filter(lambda l: not all(not bool(i) for i in l), lines)  # filter out lines where each value is ''
-        stations = [{
+    with Path("scrapers/stops.txt").open("r") as file: #the file is from the zip file "Soll-Fahrplandaten" from https://www.mvv-muenchen.de/fahrplanauskunft/fuer-entwickler/opendata/index.html
+        lines = csv.DictReader(file, delimiter=",")  
+        stations={}
+        repeat_later=[] #when parent station is not already in dict
+        for line in lines:
+            if line[TYPE]:
+                stations.setdefault(line[STATIONID],{
                     "id":line[STATIONID],
                     "name":line[NAME],
-                    "ort":line[ORT],
-                    "global-id":line[GLOBAL_ID],
-                    "lat":Decimal(line[WGS84X].replace(",", ".")),
-                    "lon":Decimal(line[WSG84Y].replace(",", ".")),
-                } for line in lines]
-    buildings = scrape_maps()
-    building_coords: list = []
-    for building in buildings:
-        # ignore entries that dont have assigned lon/lat
-        if latlonbox := building.get("latlonbox"):
-            # since lat and lon have rather long decimal points it is probably best to not use floats
-            latn, lats = Decimal(latlonbox.get("north")), Decimal(latlonbox.get("south"))
-            lone, lonw = Decimal(latlonbox["east"]), Decimal(latlonbox["west"])
-            lat: Decimal = avg(latn, lats)
-            lon: Decimal = avg(lone, lonw)
-            building_coords.append({"id": building.get("id"), "desc": building.get("desc"), "center-coord": {"lat":str(lat),"lon":str(lon)}, "public-transport":nearby((lat,lon),stations)})
-
-    return building_coords
+                    "lat":line[WGS84X].replace(",", "."),
+                    "lon":line[WSG84Y].replace(",", "."),
+                    "sub_stations":[]
+                } )
+            else:
+                if (parent:=stations.get(line[PARENT])):
+                    parent.get("sub_stations").append({
+                        "id":line[STATIONID],
+                        "name":line[NAME],
+                        "lat":line[WGS84X].replace(",", "."),
+                        "lon":line[WSG84Y].replace(",", "."),
+                        "parent":line[PARENT]
+                    })
+                else:
+                    repeat_later.append({
+                        "id":line[STATIONID],
+                        "name":line[NAME],
+                        "lat":line[WGS84X].replace(",", "."),
+                        "lon":line[WSG84Y].replace(",", "."),
+                        "parent":line[PARENT]
+                    })
+        for sub in repeat_later:
+            if (parent:=stations.get(sub["parent"])):
+                parent.get("sub_stations").append(sub)
+        return stations
