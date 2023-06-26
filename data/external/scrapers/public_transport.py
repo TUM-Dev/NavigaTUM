2b55dceac52e11c187faf2f102594a6ef4907966
quarz12 <danielmartinqwe@gmail.com>
2023-06-26 22:36:30 +0200

    automatic download for stops.txt

--- data/external/scrapers/public_transport.py
+++ data/external/scrapers/public_transport.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from external.scraping_utils import cached_json
+from external.scraping_utils import cached_json, _download_file,CACHE_PATH
+from zipfile import ZipFile
 import csv
 # CSV indexes
 STATIONID = "stop_id"
@@ -9,10 +10,24 @@ LATITUDE = "stop_lat"
 LONGITUDE = "stop_lon"
 PARENT="parent_station"
 
+def _download_zip(filepath):
+    url="https://www.mvv-muenchen.de/fileadmin/mediapool/02-Fahrplanauskunft/03-Downloads/openData/mvv_gtfs.zip"
+    _download_file(url,filepath)
+
+def _extract_stops(zip_location,target_dir):
+    for file in (zip:=ZipFile(zip_location)).infolist():
+        if file.filename=="stops.txt":
+            zip.extract(file,target_dir)
+            return
+    raise FileNotFoundError(f"could not find stops.txt in {zip_location}")
 
 @cached_json("public_transport.json")
 def scrape_stations():
-    with Path("scrapers/stops.txt").open("r") as file: #the file is from the zip file "Soll-Fahrplandaten" from https://www.mvv-muenchen.de/fahrplanauskunft/fuer-entwickler/opendata/index.html
+    parent_dir=CACHE_PATH / "public_transport"
+    _download_zip(parent_dir / "fahrplandaten.zip")
+    _extract_stops(parent_dir / "fahrplandaten.zip", parent_dir)
+
+    with Path(parent_dir / "stops.txt").open("r") as file: #the file is from the zip file "Soll-Fahrplandaten" from https://www.mvv-muenchen.de/fahrplanauskunft/fuer-entwickler/opendata/index.html
         lines = csv.DictReader(file, delimiter=",")  
         stations={}
         repeat_later=[] #when parent station is not already in dict
