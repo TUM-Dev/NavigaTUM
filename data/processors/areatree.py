286c43c6f10e1ffbce42ab87f7f228be27f537a3
Frank Elsinga <frank@elsinga.de>
2023-07-04 02:28:32 +0200

    more extraction

--- data/processors/areatree.py
+++ data/processors/areatree.py
@@ -9,7 +9,7 @@ def read_areatree():
 
     # The first line is extracted as mypy cannot make sense of this otherwise
     lines = _areatree_lines()
-    last_element:str = _parse_areatree_line(next(lines))["id"]
+    last_element: str = _parse_areatree_line(next(lines))["id"]
     for line in lines:
         indent = len(line) - len(line.lstrip(" "))
         if indent % 2 != 0:
@@ -19,7 +19,8 @@ def read_areatree():
         elif (indent // 2) < len(parent_stack):
             parent_stack = parent_stack[: indent // 2]
 
-        last_element,building_data = _parse_areatree_line(line)
+        building_data = _parse_areatree_line(line)
+        last_element = building_data["id"]
         data[building_data["id"]] = building_data | {"parents": parent_stack[:]}
     return data
 
@@ -62,37 +63,37 @@ def _parse_areatree_line(line: str) -> dict:
     """Parses a line from the areatree file to reveal the correct parent and children"""
     (building_ids, raw_names, internal_id) = _split_line(line)
 
-    building_data = _extract_building_data(building_ids)
+    building_data = _extract_building_prefix(building_ids)
     building_data |= _extract_names(raw_names.split("|"))
-
-    # id and type
-    if "[" in internal_id:
-        internal_id, building_data["type"] = internal_id.removesuffix("]").split("[")
-
-    if "," in internal_id:
-        ids = internal_id.split(",")
-        if len(ids) != 2:
-            raise RuntimeError(f"More than two ids found: '{line}'")
-        building_data["id"], building_data["visible-id"] = ids
-    elif internal_id:
-        building_data["id"] = internal_id
-    elif isinstance(building_data["b_prefix"], str) and building_data["b_prefix"]:
-        building_data["id"] = building_ids
-
-    if "id" not in building_data:
-        raise RuntimeError(f"No id provided in line: '{line}'")
-
-    # we infer which type some elements are, if they have not specified it
-    if "type" not in building_data:
-        if "b_prefix" in building_data and building_data["id"] == building_data["b_prefix"]:
-            building_data["type"] = "building"
-        else:
-            building_data["type"] = "area"
+    building_data |= _extract_id_and_type(internal_id, line, building_data.get("b_prefix"))
 
     return building_data
 
 
-def _extract_building_data(building_ids:str)->dict:
+def _extract_id_and_type(internal_id, line, b_prefix: str | list[str] | None):
+    """Extracts the id and type from the internal_id"""
+    results = {}
+    if "[" in internal_id:
+        internal_id, results["type"] = internal_id.removesuffix("]").split("[")
+    if "," in internal_id:
+        ids = internal_id.split(",")
+        if len(ids) != 2:
+            raise RuntimeError(f"More than two ids found: '{line}'")
+        results["id"], results["visible-id"] = ids
+    elif internal_id:
+        results["id"] = internal_id
+    elif isinstance(b_prefix, str) and b_prefix:
+        results["id"] = b_prefix
+    if "id" not in results:
+        raise RuntimeError(f"No id provided in line: '{line}'")
+    # we infer which type some elements are, if they have not specified it
+    if "type" not in results:
+        results["type"] = "building" if results["id"] == b_prefix else "area"
+    return results
+
+
+def _extract_building_prefix(building_ids: str) -> dict:
+    """Extracts the building prefix from the building_ids"""
     results = {}
     # areatree_uncertain
     if "-" in building_ids:
@@ -108,6 +109,7 @@ def _extract_building_data(building_ids:str)->dict:
 
 
 def _extract_names(names: list[str]) -> dict[str, str]:
+    """Extracts the name and the possible short_name"""
     building_data = {"name": names[0]}
     if len(names) == 2:
         if len(names[1]) > 20:
