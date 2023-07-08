ae5fd5d2778c8eda0ffe3214c12c9349629f29c9
Frank Elsinga <frank@elsinga.de>
2023-07-09 01:12:06 +0200

    used a different command for linting and formatting See https://github.com/vuejs/eslint-config-prettier#use-separate-commands-for-linting-and-formatting Fixes https://github.com/vuejs/eslint-config-prettier/issues/15#issuecomment-1622040807

--- webclient/.eslintrc.cjs
+++ webclient/.eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
+    "@vue/eslint-config-prettier/skip-formatting",
   ],
   parserOptions: {
     ecmaVersion: "latest",
