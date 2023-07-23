9b180212e1e6a72d9b5f094b3b9b2e3cfab0eaee
Frank Elsinga <frank@elsinga.de>
2023-07-23 03:53:29 +0200

    [ci-2] Webclient CI (#711)
    * added a way to test the webclient in CI

--- webclient/cypress/e2e/frontpage.cy.ts
+++ webclient/cypress/e2e/frontpage.cy.ts
@@ -1,17 +1,23 @@
 describe("Check if navigating from the frontpage works as expected", () => {
   it("navigating to the mi", () => {
+    cy.intercept("GET", "/api/get/root?lang=de", { fixture: "get/root.de.json" });
     cy.visit("http://localhost:8000/");
+    cy.intercept("GET", "/api/get/mi?lang=de", { fixture: "get/mi.de.json" });
     cy.contains("Informatik").click();
     cy.url().should("include", "/building/mi");
   });
   it("navigating to an initally hidden entry", () => {
+    cy.intercept("GET", "/api/get/root?lang=de", { fixture: "get/root.de.json" });
     cy.visit("http://localhost:8000/");
     cy.contains("mehr").click();
+    cy.intercept("GET", "/api/get/garching-interims?lang=de", { fixture: "get/garching-interims.de.json" });
     cy.contains("Interims").click();
-    cy.url().should("include", "/site/garching-interims");
+    cy.url().should("include", "/site/");
   });
   it("navigate to an campus", () => {
+    cy.intercept("GET", "/api/get/root?lang=de", { fixture: "get/root.de.json" });
     cy.visit("http://localhost:8000/");
+    cy.intercept("GET", "/api/get/garching?lang=de", { fixture: "get/garching.de.json" });
     cy.contains("Garching Forschungszentrum").click({ scrollBehavior: false });
     cy.url().should("include", "/campus/garching");
   });
