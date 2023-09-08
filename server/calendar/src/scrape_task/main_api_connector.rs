2fe3b7c21ea9e7f1b61d9e2a5926a155bac85950
Frank Elsinga <frank@elsinga.de>
2023-09-08 13:06:06 +0200

    vectorised the api_data

--- server/calendar/src/scrape_task/main_api_connector.rs
+++ server/calendar/src/scrape_task/main_api_connector.rs
@@ -16,6 +16,7 @@ fn api_url_from_env() -> Option<String> {
 
 #[derive(Deserialize, Debug)]
 pub struct ReducedRoom {
+    key: String,
     props: ReducedRoomProps,
 }
 
@@ -34,12 +35,12 @@ pub struct Room {
 }
 
 impl Room {
-    fn from((key, room): (String, ReducedRoom)) -> Option<Room> {
+    fn from(room: ReducedRoom) -> Option<Room> {
         let url = room.props.calendar_url?;
         let regex = Regex::new(r".*cOrg=(?P<org>\d+)&cRes=(?P<cal>\d+)\D.*").unwrap();
         let captures = regex.captures(&url)?;
         Some(Room {
-            sap_id: key,
+            sap_id: room.key,
             tumonline_org_id: captures.name("org")?.as_str().parse().ok()?,
             tumonline_calendar_id: captures.name("cal")?.as_str().parse().ok()?,
             tumonline_room_id: room.props.tumonline_room_nr?,
@@ -52,7 +53,7 @@ pub async fn get_all_ids() -> Vec<Room> {
         api_url_from_env().unwrap_or_else(|| "https://nav.tum.de/cdn/api_data.json".to_string());
     let res = reqwest::get(&url).await;
     let rooms = match res {
-        Ok(res) => res.json::<HashMap<String, ReducedRoom>>().await,
+        Ok(res) => res.json::<Vec<ReducedRoom>>().await,
         Err(e) => {
             error!("Failed to contact main-api at {url}: {e:#?}");
             return vec![];
