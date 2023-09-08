5627640d42a2aa3eeff2960fce335eaca8f7896a
Frank Elsinga <frank@elsinga.de>
2023-09-08 21:10:57 +0200

    tmp

--- server/main-api/src/setup/mod.rs
+++ server/main-api/src/setup/mod.rs
@@ -1,13 +1,11 @@
 mod database;
 
-#[cfg(skip_meilisearch)]
 mod meilisearch;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
-    #[cfg(skip_meilisearch)]
     meilisearch::setup_meilisearch().await?;
     database::setup_database().await?;
     Ok(())
