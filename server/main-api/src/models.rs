daad7fe0e8f14dd8de83b3105001873f3ce5db58
Frank Elsinga <frank@elsinga.de>
2023-09-29 16:07:19 +0200

    chore:migrated the main api to sqlx (#825)
    * migrated the main api to sqlx
    
    * fixed the lockfile

--- server/main-api/src/models.rs
+++ server/main-api/src/models.rs
@@ -1,20 +1,11 @@
-use diesel::prelude::*;
-
-#[derive(Queryable, Debug, Clone)]
+#[derive(Debug, Clone)]
 pub struct DBRoomEntry {
     pub key: String,
     pub name: String,
-    pub tumonline_room_nr: Option<i32>,
-    pub type_: String,
+    pub tumonline_room_nr: Option<i64>,
+    pub r#type: String,
     pub type_common_name: String,
-    pub lat: f32,
-    pub lon: f32,
+    pub lat: f64,
+    pub lon: f64,
     pub data: String,
-}
-
-#[derive(Queryable, Debug, Clone)]
-pub struct DBRoomKeyAlias {
-    pub key: String,
-    pub visible_id: String,
-    pub type_: String,
 }
