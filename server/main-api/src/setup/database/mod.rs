a7f68387f719dfd9f57ae6318bded586acc001ff
Frank Elsinga <frank@elsinga.de>
2023-11-03 00:48:45 +0100

    tmp

--- server/main-api/src/setup/database/mod.rs
+++ server/main-api/src/setup/database/mod.rs
@@ -1,25 +1,21 @@
 mod alias;
 mod data;
 
-use sqlx::sqlite::SqlitePoolOptions;
-use sqlx::Executor;
+use sqlx::{Executor, SqlitePool};
 
-pub(crate) async fn setup_database() -> Result<(), Box<dyn std::error::Error>> {
-    let uri = std::env::var("DB_LOCATION").unwrap_or_else(|_| "api_data.db".to_string());
-    let uri = format!("{uri}?mode=rwc");
-    let pool = SqlitePoolOptions::new().connect(&uri).await?;
-    sqlx::migrate!("./migrations").run(&pool).await?;
+pub(crate) async fn setup_database(pool: &SqlitePool) -> Result<(), Box<dyn std::error::Error>> {
+    sqlx::migrate!("./migrations").run(pool).await?;
     // this is to setup the database faster
     // we don't want to use an acid compliant database for this step ;)
     pool.execute("PRAGMA journal_mode = OFF;");
     pool.execute("PRAGMA synchronous = OFF;");
 
     // delete all old data
-    sqlx::query!("DELETE FROM aliases").execute(&pool).await?;
-    sqlx::query!("DELETE FROM en").execute(&pool).await?;
-    sqlx::query!("DELETE FROM de").execute(&pool).await?;
+    sqlx::query!("DELETE FROM aliases").execute(pool).await?;
+    sqlx::query!("DELETE FROM en").execute(pool).await?;
+    sqlx::query!("DELETE FROM de").execute(pool).await?;
 
-    data::load_all_to_db(&pool).await?;
-    alias::load_all_to_db(&pool).await?;
+    data::load_all_to_db(pool).await?;
+    alias::load_all_to_db(pool).await?;
     Ok(())
 }
