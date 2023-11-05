55a2cc712c32e4dcb56fca43c6c6318ca5e87b2e
Frank Elsinga <frank@elsinga.de>
2023-11-06 00:27:08 +0100

    tmp

--- server/main-api/tests/query.rs
+++ server/main-api/tests/query.rs
@@ -1,15 +1,7 @@
-use std::usize;
 use serde::Deserialize;
+use std::usize;
 
-
-#[derive(Debug, Eq, PartialEq, Hash)]
-struct Query {
-    target: String,
-    query: String,
-    among: Option<usize>,
-}
-
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 struct SearchResult {
     queried_length: usize,
     target_pos: Option<usize>,
@@ -47,20 +39,13 @@ struct Evaluation {
 }
 
 #[derive(Debug, Clone, Deserialize)]
-struct EvaluatableQuery {
+struct Query {
     target: String,
     query: String,
     among: Option<usize>,
 }
-#[derive(Debug,  Deserialize)]
-struct SearchItem {
-    sections: Vec<SearchSectionItem>,
-}
-#[derive(Debug, Deserialize)]
-struct SearchSectionItem {
-    id: String,
-}
-impl EvaluatableQuery {
+
+impl Query {
     async fn do_search(&self, url: &str, queried_length: usize) -> SearchResult {
         let req = reqwest::get(url)
             .await
@@ -69,55 +54,76 @@ impl EvaluatableQuery {
             .await
             .expect("Failed to parse JSON response");
 
-        let hits: Vec<serde_json::Value> = req["sections"].as_array()
+        let hits: Vec<serde_json::Value> = req["sections"]
+            .as_array()
             .unwrap_or(&vec![])
             .iter()
             .flat_map(|s| s["entries"].as_array().unwrap_or(&vec![]).to_vec())
             .collect();
 
-        let target_pos = hits.iter().position(|s| s["id"].as_str() == Some(&self.target));
+        let target_pos = hits
+            .iter()
+            .position(|s| s["id"].as_str() == Some(&self.target));
 
         SearchResult {
             queried_length,
             target_pos,
-            estimated_total_hits_sum: req["sections"].as_array()
-                .map(|sections| sections.iter().map(|s| s["estimatedTotalHits"].as_i64().unwrap_or(0)).sum())
+            estimated_total_hits_sum: req["sections"]
+                .as_array()
+                .map(|sections| {
+                    sections
+                        .iter()
+                        .map(|s| s["estimatedTotalHits"].as_i64().unwrap_or(0))
+                        .sum()
+                })
                 .unwrap_or(0) as i32,
         }
     }
 
-    async fn evaluate(&self, search_endpoint: &str) -> Evaluation {
-        let mut searches: Vec<SearchResult>=vec![];
+    async fn evaluate(self, search_endpoint: &str) -> Evaluation {
+        let mut searches: Vec<SearchResult> = vec![];
         for query_length in 1..=self.query.len() {
-            let url=format!("{search_endpoint}/?q={}", &self.query[..query_length]);
-            searches.push( self.do_search(&url, query_length).await);
+            let url = format!("{search_endpoint}/?q={}", &self.query[..query_length]);
+            searches.push(self.do_search(&url, query_length).await);
         }
 
         let final_search = searches.last().unwrap().clone();
-        let successfull_searches: Vec<SearchResult> = searches.into_iter().filter(|search| search.was_successful()).collect();
+        let successfull_searches: Vec<SearchResult> = searches
+            .into_iter()
+            .filter(|search| search.was_successful())
+            .collect();
 
-        let best_search = successfull_searches.iter()
-            .min_by_key(|&search| (search.target_pos.unwrap_or(usize::MAX), search.queried_length))
+        let best_search = successfull_searches
+            .iter()
+            .min_by_key(|&search| {
+                (
+                    search.target_pos.unwrap_or(usize::MAX),
+                    search.queried_length,
+                )
+            })
             .cloned();
 
-        let searches_who_reached_top_5: Vec<SearchResult> = successfull_searches.clone().into_iter()
+        let searches_who_reached_top_5: Vec<SearchResult> = successfull_searches
+            .clone()
+            .into_iter()
             .filter(|search| search.was_top5())
             .collect();
 
-        let len_to_reach_top_5 = searches_who_reached_top_5.first().map(|search| search.queried_length);
+        let len_to_reach_top_5 = searches_who_reached_top_5
+            .first()
+            .map(|search| search.queried_length);
 
-        let len_to_reach_final = successfull_searches.iter()
+        let len_to_reach_final = successfull_searches
+            .iter()
             .filter(|&search| search.target_pos == final_search.target_pos)
             .map(|search| search.queried_length)
             .min();
 
         Evaluation {
-            query: Query {
-                target: self.target.clone(),
-                query: self.query.clone(),
-                among: self.among,
-            },
-            best_pos: best_search.clone().map(|search| search.target_pos.unwrap_or(usize::MAX)),
+            query: self,
+            best_pos: best_search
+                .clone()
+                .map(|search| search.target_pos.unwrap_or(usize::MAX)),
             len_to_reach_top_5,
             len_to_reach_final,
             len_to_best_pos: best_search.map(|search| search.queried_length),
@@ -130,37 +136,17 @@ impl EvaluatableQuery {
 mod tests {
     use super::*;
 
+    fn queries() -> Vec<Query> {
+        let test_queries = include_str!("test-queries.yaml");
+        serde_yaml::from_str::<Vec<Query>>(&test_queries)
+            .expect("Failed to parse test-queries.yaml")
+    }
     #[tokio::test]
     async fn evaluate_queries() {
         let search_endpoint = "https://nav.tum.de/api/search";
-        let evaluations = Evaluations::new(search_endpoint);
-        for evaluation in evaluations.results().await {
-            println!("{:?}", evaluation);
+        for query in queries() {
+            let evaluation = query.evaluate(search_endpoint).await;
+            println!("{evaluation:?}");
         }
-    }
-}
-
-struct Evaluations {
-    search_endpoint: String,
-}
-
-impl Evaluations {
-    fn new(search_endpoint: &str) -> Self {
-        Self {
-            search_endpoint: search_endpoint.to_string(),
-        }
-    }
-
-    fn queries(&self) -> Vec<EvaluatableQuery> {
-        let test_queries = include_str!("test-queries.yaml");
-        serde_yaml::from_str::<Vec<EvaluatableQuery>>(&test_queries).expect("Failed to parse test-queries.yaml")
-    }
-
-    async fn results(&self) -> Vec<Evaluation> {
-        let mut results =vec![];
-        for query in self.queries() {
-            results.push(query.evaluate(&self.search_endpoint).await);
-        }
-        results
     }
 }
