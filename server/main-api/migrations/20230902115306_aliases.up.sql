d58f2a721d22c777c8fe1e95d0158ede687bcddd
Frank Elsinga <frank@elsinga.de>
2023-11-13 12:15:23 +0100

    fixed the aliases not being correctly indexed

--- server/main-api/migrations/20230902115306_aliases.up.sql
+++ server/main-api/migrations/20230902115306_aliases.up.sql
@@ -2,9 +2,11 @@
 CREATE TABLE aliases
 (
     id         SERIAL PRIMARY KEY NOT NULL,
-    alias      TEXT UNIQUE                       NOT NULL,
-    key        TEXT                              NOT NULL,
-    visible_id TEXT                              NOT NULL,
-    type       TEXT                              NOT NULL,
+    alias      TEXT   NOT NULL,
+    key        TEXT   NOT NULL,
+    visible_id TEXT   NOT NULL,
+    type       TEXT   NOT NULL,
     FOREIGN KEY(key) REFERENCES de(key)
 );
+
+CREATE UNIQUE INDEX aliases_alias_key_uindex ON aliases (alias, key);
