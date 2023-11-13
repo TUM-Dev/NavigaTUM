25a4356961332101259ab13593eb1ffdd3bd6c4b
Frank Elsinga <frank@elsinga.de>
2023-11-13 01:35:01 +0100

    added more logging information to the db

--- server/main-api/src/setup/database/mod.rs
+++ server/main-api/src/setup/database/mod.rs
@@ -1,21 +1,20 @@
 mod alias;
 mod data;
 
-use sqlx::{Executor, PgPool};
+use log::info;
+use sqlx::PgPool;
 
 pub(crate) async fn setup_database(pool: &PgPool) -> Result<(), Box<dyn std::error::Error>> {
     sqlx::migrate!("./migrations").run(pool).await?;
-    // this is to setup the database faster
-    // we don't want to use an acid compliant database for this step ;)
-    pool.execute("PRAGMA journal_mode = OFF;");
-    pool.execute("PRAGMA synchronous = OFF;");
 
-    // delete all old data
+    info!("database setup complete, deleting old data");
     sqlx::query!("DELETE FROM aliases").execute(pool).await?;
     sqlx::query!("DELETE FROM en").execute(pool).await?;
     sqlx::query!("DELETE FROM de").execute(pool).await?;
 
+    info!("loading new data");
     data::load_all_to_db(pool).await?;
+    info!("loading new aliases");
     alias::load_all_to_db(pool).await?;
     Ok(())
 }
