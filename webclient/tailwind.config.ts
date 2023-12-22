0eded70abe9ae2dea1f4ef443ed4858b862d2424
Frank Elsinga <frank@elsinga.de>
2023-12-22 06:22:56 +0100

    simplified the tailwind config

---
+++ webclient/tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+export default <Partial<Config>>{
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        tumBlue: {
+          50: "#f3f6fc",
+          100: "#e5edf9",
+          200: "#c6d9f1",
+          300: "#93bae6",
+          400: "#5a97d6",
+          500: "#3070b3",
+          600: "#245fa5",
+          700: "#1f4d85",
+          800: "#1d426f",
+          900: "#1d385d",
+          950: "#13243e",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
