f59b392b8b2e18c9c94a978ac29adaece32baeb7
Frank Elsinga <frank@elsinga.de>
2023-12-26 09:42:46 +0100

    migrated the 404 page to tailwind

--- webclient/src/components/Button.vue
+++ webclient/src/components/Button.vue
@@ -4,14 +4,16 @@ import { computed } from "vue";
 export interface Props {
   type?: "submit" | "reset" | "button";
   size?: "sm" | "md" | "lg";
+  to?: string;
   disabled?: boolean;
-  variant?: "action" | "base" | "info" | "primary";
+  variant?: "action" | "link" | "base" | "info" | "primary";
   ariaLabel?: string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
   type: "button",
   size: "md",
+  to: "",
   variant: "base",
   ariaLabel: "",
   disabled: false,
@@ -22,7 +24,9 @@ const variantClasses = computed(() => {
     case "action":
       return "";
     case "primary":
-      return "bg-tumBlue-500 text-tumBlue-50 hover:bg-tumBlue-600 hover:text-white";
+      return "bg-tumBlue-500 visited:text-tumBlue-50 text-tumBlue-50 hover:bg-tumBlue-600 hover:text-white";
+    case "link":
+      return "bg-transparent visited:text-tumBlue-600 text-tumBlue-600 hover:bg-tumBlue-100 hover:text-tumBlue-500";
     case "base":
       return "";
     default:
@@ -34,7 +38,7 @@ const sizeClasses = computed(() => {
     case "sm":
       return "text-md px-1.5 rounded-xs";
     case "md":
-      return "text-md px-1.5 rounded-sm";
+      return "text-md px-4 py-1.5 rounded-sm";
     case "lg":
       return "text-lg px-2.5 rounded-md";
     default:
@@ -44,11 +48,23 @@ const sizeClasses = computed(() => {
 </script>
 
 <template>
-  <button
+  <RouterLink
+    v-if="props.to.length"
+    :to="props.to"
     :aria-label="ariaLabel"
     :type="props.type"
     v-bind="{ disabled: disabled }"
-    :class="`${variantClasses} ${sizeClasses}`"
+    :class="`!no-underline flex-row flex gap-1 ${variantClasses} ${sizeClasses}`"
+    @click="emit('click')"
+  >
+    <slot />
+  </RouterLink>
+  <button
+    v-else
+    :aria-label="ariaLabel"
+    :type="props.type"
+    v-bind="{ disabled: disabled }"
+    :class="`flex-row flex gap-1 ${variantClasses} ${sizeClasses}`"
     @click="emit('click')"
   >
     <slot />
--- webclient/src/pages/[...slug].vue
+++ webclient/src/pages/[...slug].vue
@@ -2,39 +2,46 @@
 import { useGlobalStore } from "@/stores/global";
 import { useRoute } from "vue-router";
 import { useI18n } from "vue-i18n";
-
 const route = useRoute();
+import { ArrowRightIcon } from "@heroicons/vue/24/outline";
+import Button from "@/components/Button.vue";
 const global = useGlobalStore();
 const { t } = useI18n({ useScope: "local" });
 </script>
 
 <template>
-  <Toast level="warning" :msg="t('error_404')" />
-  <h5 style="margin-top: 25px">{{ t("header") }}</h5>
-  <p>{{ t("description") }}</p>
-  <button
-    type="button"
-    class="btn btn-link p-0"
-    :aria-label="t('open_feedback')"
-    @click="global.openFeedback('bug', `404 on ${route.fullPath}`, t('got_here'))"
-  >
-    {{ t("call_to_action") }}
-  </button>
+  <div class="mt-4 max-w-xl mx-auto">
+    <img class="" src="@/assets/404_navigatum.svg" :alt="t('img_alt')" />
+    <div>
+      <h5 class="mt-5">{{ t("header") }}</h5>
+      <p>{{ t("description") }}</p>
+      <div class="flex flex-row gap-4">
+        <Button to="/" variant="primary">
+          {{ t("go_home") }}
+        </Button>
+        <Button variant="link" @click="global.openFeedback('bug', `404 on \`${route.fullPath}\``, t('got_here'))">
+          {{ t("call_to_action") }} <ArrowRightIcon class="h-4 w-4 mt-0.5" />
+        </Button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <i18n lang="yaml">
 de:
-  error_404: 404 Nicht gefunden
-  call_to_action: Falls du denkst, dass dies ein Fehler ist, teile es uns doch hier mit
+  open_feedback: Open the feedback-form
   description: Dies könnte sein, weil wir einen Fehler gemacht haben.
   got_here: "Ich habe diesen Fehler so gefunden:\r\n1. ..."
   header: Die angeforderte Seite wurde nicht gefunden.
-  open_feedback: Open the feedback-form
+  img_alt: Illustration einer männlichen Person, die auf großen '404'-Buchstaben sitzt und einer weiblichen person vor dem TUM Hauptgebäude
+  go_home: Zurück gehen
+  call_to_action: Feedback geben
 en:
-  error_404: 404 Not found
-  call_to_action: If you think this is a mistake, please let us know here
+  open_feedback: Open the feedback-form
   description: This could be because we made a mistake.
   got_here: "I have found the error by:\r\n1. ..."
   header: The requested website could not to be found.
-  open_feedback: Open the feedback-form
+  img_alt: illustration of a female and male person sitting on large '404'-letters in front of the tum main building
+  go_home: Go back home
+  call_to_action: Give Feedback
 </i18n>
