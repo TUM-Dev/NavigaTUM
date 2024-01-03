dcbb1a92f31adf707ae0d746dff8377a26c5da24
Frank Elsinga <frank@elsinga.de>
2024-01-03 18:53:41 +0100

    migrated to pytest

--- data/external/models/test_models.py
+++ data/external/models/test_models.py
@@ -1,46 +1,56 @@
-import unittest
+from typing import Literal
+
+import pytest
 
 from external.models import nat, public_transport, roomfinder, tumonline
 
 
-class ModelLoader(unittest.TestCase):
-    def test_nat(self):
-        """Test if the nat models can be loaded correctly"""
-        with self.subTest(nat.Building):
-            nat.Building.load_all()
-        with self.subTest(nat.Room):
-            nat.Room.load_all()
-        with self.subTest(nat.Campus):
-            nat.Campus.load_all()
-        with self.subTest(nat.Organisation):
-            nat.Organisation.load_all()
-
-    def test_roomfinder(self):
-        """Test if the roomfinder models can be loaded correctly"""
-        with self.subTest(roomfinder.Map):
-            roomfinder.Map.load_all()
-        with self.subTest(roomfinder.Room):
-            roomfinder.Room.load_all()
-        with self.subTest(roomfinder.Building):
-            roomfinder.Building.load_all()
-
-    def test_tumonline(self):
-        """Test if the tumonline models can be loaded correctly"""
-        with self.subTest(tumonline.Room):
-            tumonline.Room.load_all()
-        with self.subTest(tumonline.Building):
-            tumonline.Building.load_all()
-        for lang in ("de", "en"):
-            with self.subTest(tumonline.Organisation, lang=lang):
-                tumonline.Organisation.load_all_for(lang)
-        with self.subTest(tumonline.Usage):
-            tumonline.Usage.load_all()
-
-    def test_public_transport(self):
-        """Test if the public_transport models can be loaded correctly"""
-        with self.subTest(public_transport.Station):
-            public_transport.Station.load_all()
+def test_nat_building():
+    nat.Building.load_all()
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_nat_room():
+    nat.Room.load_all()
+
+
+def test_nat_campus():
+    nat.Campus.load_all()
+
+
+def test_nat_org():
+    nat.Organisation.load_all()
+
+
+def test_roomfinder_map():
+    """Test if the roomfinder.Map model can be loaded correctly"""
+    roomfinder.Map.load_all()
+
+
+def test_roomfinder_room():
+    """Test if the roomfinder.Room model can be loaded correctly"""
+    roomfinder.Room.load_all()
+
+
+def test_roomfinder_building():
+    roomfinder.Building.load_all()
+
+
+def test_tumonline_room():
+    tumonline.Room.load_all()
+
+
+def test_tumonline_room():
+    tumonline.Building.load_all()
+
+
+@pytest.mark.parametrize("lang", ["de", "en"])
+def test_tumonline_room(lang: Literal["de", "en"]):
+    tumonline.Organisation.load_all_for(lang)
+
+
+def test_tumonline_usage():
+    tumonline.Usage.load_all()
+
+
+def test_public_transport():
+    public_transport.Station.load_all()
