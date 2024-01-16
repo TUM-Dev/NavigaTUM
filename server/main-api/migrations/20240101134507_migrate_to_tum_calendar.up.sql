3154a3a84cd7c5a8a3735c4aa011f27d35d8a1b2
Frank Elsinga <frank@elsinga.de>
2024-01-16 13:55:52 +0100

    added a db migration

---
+++ server/main-api/migrations/20240101134507_migrate_to_tum_calendar.up.sql
@@ -0,0 +1,18 @@
+-- Your SQL goes here
+DROP TABLE IF EXISTS calendar;
+DROP INDEX IF EXISTS calendar_lut;
+
+CREATE TYPE EventType AS ENUM ('lecture','exercise','exam','barred','other');
+CREATE TABLE calendar
+(
+    id                  INTEGER UNIQUE PRIMARY KEY NOT NULL,
+    room_code           VARCHAR(30)                NOT NULL,
+    start_at            timestamp                  NOT NULL,
+    end_at              timestamp                  NOT NULL,
+    stp_title_de        TEXT                       NOT NULL,
+    stp_title_en        TEXT                       NOT NULL,
+    stp_type            TEXT                       NOT NULL,
+    entry_type          EventType                  NOT NULL,
+    detailed_entry_type TEXT                       NOT NULL
+);
+CREATE INDEX IF NOT EXISTS calendar_lut ON calendar (room_code, start_at, end_at);
