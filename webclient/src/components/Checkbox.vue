059b42245485e2d1978f11ed844b0dbdef1cbd66
Frank Elsinga <frank@elsinga.de>
2024-02-28 00:49:05 +0100

    fixed spacing in the feedback form

--- webclient/src/components/Checkbox.vue
+++ webclient/src/components/Checkbox.vue
@@ -7,7 +7,7 @@ const model = defineModel<boolean>();
 
 <template>
   <div class="mb-4 flex gap-2">
-    <div class="flex h-5 items-center">
+    <div class="flex h-5 min-w-4 items-center">
       <input
         :id="id"
         v-model="model"
@@ -15,7 +15,7 @@ const model = defineModel<boolean>();
         class="focusable text-tumBlue-600 bg-zinc-100 border-zinc-300 z-40 min-h-4 min-w-4 rounded"
       />
     </div>
-    <div class="ms-2">
+    <div class="ps-1">
       <label :for="id" class="text-zinc-900 z-30 text-sm font-medium">
         <slot />
       </label>
