98dde19f07a374e324d25a8b2f60356f9ac490fb
Frank Elsinga <frank@elsinga.de>
2024-03-03 20:55:30 +0100

    generic sources

--- webclient/src/components/DetailsSources.vue
+++ webclient/src/components/DetailsSources.vue
@@ -8,47 +8,49 @@ const { t } = useI18n({ useScope: "local" });
 
 <template>
   <section>
-    <h2 class="text-zinc-800 text-lg font-semibold">{{ t("title") }}</h2>
-    <p v-if="state.data?.sources.base" class="text-zinc-600">
-      {{ t("base.title") }}:
-      <span v-for="(e, i) in state.data.sources.base" :key="e.name">
-        <a v-if="e.url" :href="e.url">{{ e.name }}</a>
-        <template v-else>{{ e.name }}</template>
-        <template v-if="i < state.data.sources.base.length - 1"> • </template>
-      </span>
-      <span v-if="state.data.sources.patched">
-        <br />
-        ({{ t("base.patched") }})
-      </span>
-    </p>
-    <p v-if="state.image.shown_image" class="text-zinc-600">
-      {{ t("header_img") }}:
-      <span>{{ state.image.shown_image.author.text }}</span>
-      <span v-if="state.image.shown_image.source">
-        •
-        <a v-if="state.image.shown_image.source.url" :href="state.image.shown_image.source.url" target="_blank">
-          {{ state.image.shown_image.source.text }}
-        </a>
-        <template v-else>{{ state.image.shown_image.source.text }}</template>
-      </span>
-      <span v-if="state.image.shown_image.license">
-        •
-        <a v-if="state.image.shown_image.license.url" :href="state.image.shown_image.license.url" target="_blank">
-          {{ state.image.shown_image.license.text }}
-        </a>
-        <template v-else>{{ state.image.shown_image.license.text }}</template>
-      </span>
-    </p>
-    <p v-if="state.data?.coords" class="text-zinc-600">
-      {{ t("coords.title") }}:
-      <span v-if="state.data.coords.source === 'navigatum'"> {{ t("coords.navigatum") }}</span>
-      <span v-if="state.data.coords.source === 'roomfinder'">
-        {{ t("coords.roomfinder") }}
-      </span>
-      <span v-if="state.data.coords.source === 'inferred'">
-        {{ t("coords.inferred") }}
-      </span>
-    </p>
+    <h2 class="text-md text-zinc-800 font-semibold">{{ t("title") }}</h2>
+    <div class="text-zinc-600 text-sm">
+      <p v-if="state.data?.sources.base">
+        {{ t("base.title") }}:
+        <span v-for="(e, i) in state.data.sources.base" :key="e.name">
+          <a v-if="e.url" :href="e.url">{{ e.name }}</a>
+          <template v-else>{{ e.name }}</template>
+          <template v-if="i < state.data.sources.base.length - 1"> • </template>
+        </span>
+        <span v-if="state.data.sources.patched">
+          <br />
+          ({{ t("base.patched") }})
+        </span>
+      </p>
+      <p v-if="state.image.shown_image">
+        {{ t("header_img") }}:
+        <span>{{ state.image.shown_image.author.text }}</span>
+        <span v-if="state.image.shown_image.source">
+          •
+          <a v-if="state.image.shown_image.source.url" :href="state.image.shown_image.source.url" target="_blank">
+            {{ state.image.shown_image.source.text }}
+          </a>
+          <template v-else>{{ state.image.shown_image.source.text }}</template>
+        </span>
+        <span v-if="state.image.shown_image.license">
+          •
+          <a v-if="state.image.shown_image.license.url" :href="state.image.shown_image.license.url" target="_blank">
+            {{ state.image.shown_image.license.text }}
+          </a>
+          <template v-else>{{ state.image.shown_image.license.text }}</template>
+        </span>
+      </p>
+      <p v-if="state.data?.coords">
+        {{ t("coords.title") }}:
+        <span v-if="state.data.coords.source === 'navigatum'"> {{ t("coords.navigatum") }}</span>
+        <span v-if="state.data.coords.source === 'roomfinder'">
+          {{ t("coords.roomfinder") }}
+        </span>
+        <span v-if="state.data.coords.source === 'inferred'">
+          {{ t("coords.inferred") }}
+        </span>
+      </p>
+    </div>
   </section>
 </template>
 
