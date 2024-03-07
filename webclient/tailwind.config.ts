770ef73718e10bb1239847d8cb0e872bf13bb822
Frank Elsinga <frank@elsinga.de>
2024-03-07 04:39:19 +0100

    class -> selector as by docs

--- webclient/tailwind.config.ts
+++ webclient/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default <Partial<Config>>{
-  darkMode: "class",
+  darkMode: "selector",
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
