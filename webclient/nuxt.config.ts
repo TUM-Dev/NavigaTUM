ac1e487d89e6c4fec7ca74883657f8adc87d2ca7
Frank Elsinga <frank@elsinga.de>
2024-03-31 04:38:17 +0200

    added a better favicon configuration

--- webclient/nuxt.config.ts
+++ webclient/nuxt.config.ts
@@ -11,8 +11,16 @@ export default defineNuxtConfig({
       bodyAttrs: { class: "bg-zinc-50" },
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
-      link: [{ rel: "icon", href: "/favicon.ico", sizes: "48x48" }],
+      link: [
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/favicons/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicons/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicons/favicon-16x16.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "mask-icon", href: "/favicons/safari-pinned-tab.svg", color: "#0065bd" },
+      ],
       meta: [
+        { name: "msapplication-TileColor", content: "#0065bd" },
+        { name: "theme-color", content: "#ffffff" },
         { name: "author", content: "OpenSource @ TUM e.V. partnering with TUM IT Management" },
         { name: "copyright", content: "GNU General Public License v3.0. Images may be licensed differently." },
         { name: "robots", content: "index, follow" },
