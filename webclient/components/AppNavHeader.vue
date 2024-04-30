36f5e72b0cb9f0d13b24354000f5907b2e43a358
Frank Elsinga <frank@elsinga.de>
2024-04-30 02:39:45 +0200

    improved the search bars min-width scaling

--- webclient/components/AppNavHeader.vue
+++ webclient/components/AppNavHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="shadow-black/5 dark:shadow-black/20 bg-zinc-50 fixed top-0 z-20 w-full shadow-md dark:bg-zinc-100">
     <nav class="mx-auto max-w-4xl">
-      <div class="mx-5 my-2.5 flex place-content-between content-between justify-between gap-x-10 lg:mx-10">
+      <div class="mx-3 my-2.5 flex place-content-between content-between justify-between gap-x-3 md:gap-x-10 lg:mx-10">
         <NuxtLink class="my-auto hidden lg:block print:!block" to="/">
           <img alt="Navigatum Logo" class="pt-2" width="201" height="32" src="~/assets/logos/navigatum.svg" />
         </NuxtLink>
