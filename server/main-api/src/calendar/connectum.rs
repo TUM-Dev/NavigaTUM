ec353a99b5d20ebb915260f4299e1d2c622e5359
Frank Elsinga <frank@elsinga.de>
2024-05-09 01:33:16 +0200

    Refactored the calendar to refresh continuously

--- server/main-api/src/calendar/connectum.rs
+++ server/main-api/src/calendar/connectum.rs
@@ -2,34 +2,31 @@ use std::{env, io};
 
 use cached::instant::Instant;
 use chrono::{DateTime, Utc};
-use log::{debug, error, info, warn};
+use log::{debug, error, warn};
 use oauth2::basic::{BasicClient, BasicTokenResponse};
 use oauth2::reqwest::async_http_client;
 use oauth2::url::Url;
 use oauth2::{AuthUrl, ClientId, ClientSecret, Scope, TokenResponse, TokenUrl};
 use sqlx::PgPool;
 
-use crate::calendar::fetch::CalendarEntryFetcher;
 use crate::calendar::models::Event;
 
-pub(super) struct APIRequestor {
+pub(in crate::calendar) struct APIRequestor {
     client: reqwest::Client,
     pool: PgPool,
 }
 
-impl CalendarEntryFetcher for APIRequestor {
-    fn new(pool: &PgPool, _: &Option<DateTime<Utc>>) -> Self {
+impl From<&PgPool> for APIRequestor {
+    fn from(pool: &PgPool) -> Self {
         Self {
             client: reqwest::Client::new(),
             pool: pool.clone(),
         }
     }
-    async fn fetch(
-        &self,
-        id: &str,
-        start_after: &DateTime<Utc>,
-        end_before: &DateTime<Utc>,
-    ) -> Result<super::CalendarEntries, crate::BoxedError> {
+}
+
+impl APIRequestor {
+    pub(crate) async fn refresh(&self, id: &str) -> Result<(), crate::BoxedError> {
         let sync_start = Utc::now();
         let start = Instant::now();
         // Make OAuth2 secured request
@@ -39,8 +36,8 @@ impl CalendarEntryFetcher for APIRequestor {
             .access_token()
             .secret()
             .clone();
-        let url = format!("https://campus.tum.de/tumonline/co/connectum/api/rooms/{id}/calendars");
 
+        let url = format!("https://campus.tum.de/tumonline/co/connectum/api/rooms/{id}/calendars");
         let events: Vec<Event> = self
             .client
             .get(url)
@@ -49,7 +46,7 @@ impl CalendarEntryFetcher for APIRequestor {
             .await?
             .json()
             .await?;
-        info!(
+        debug!(
             "finished fetching for {id}: {cnt} calendar events in {elapsed:?}",
             cnt = events.len(),
             elapsed = start.elapsed()
@@ -62,11 +59,7 @@ impl CalendarEntryFetcher for APIRequestor {
             })
             .collect::<Vec<Event>>();
         self.store(&events, &sync_start, id).await?;
-        let events = events
-            .into_iter()
-            .filter(|e| *start_after <= e.start_at && *end_before >= e.end_at)
-            .collect();
-        Ok((sync_start, events))
+        Ok(())
     }
 }
 
