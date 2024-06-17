16709db3ff43a6d4c9831db03df00ba227976c62
Frank Elsinga <frank@elsinga.de>
2024-06-17 18:59:47 +0200

    made sure that clippy does not panic on having non-existant "dead code"

--- server/main-api/src/models.rs
+++ server/main-api/src/models.rs
@@ -16,6 +16,7 @@ pub struct Location {
 }
 
 #[derive(Debug, Clone)]
+#[allow(dead_code)] // false positive. Clippy can't detect this due to macros
 pub struct LocationKeyAlias {
     pub key: String,
     pub visible_id: String,
