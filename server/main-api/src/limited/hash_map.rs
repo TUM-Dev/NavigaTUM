2d4144ecb21b7ea28857d98334c1b518eefbc8b8
Frank Elsinga <frank@elsinga.de>
2024-07-01 18:14:50 +0200

    fixed test

--- server/main-api/src/limited/hash_map.rs
+++ server/main-api/src/limited/hash_map.rs
@@ -16,18 +16,20 @@ impl<K: Eq + Hash, V> From<HashMap<K, V>> for LimitedHashMap<K, V> {
 }
 
 const LIMIT: usize = 3;
-impl<K: fmt::Debug + Eq + Hash, V: fmt::Debug> fmt::Debug for LimitedHashMap<K, V> {
+impl<K: fmt::Debug + Eq + Hash +Clone+Ord, V: fmt::Debug+Clone> fmt::Debug for LimitedHashMap<K, V> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut collection = self.0.clone().into_iter().collect::<Vec<(K,V)>>();
+        collection.sort_unstable_by(|(k1,_),(k2,_)|k1.cmp(k2));
         if self.0.len() <= LIMIT {
-            f.debug_list().entries(self.0.iter().take(LIMIT)).finish()
+            f.debug_map().entries(collection).finish()
         } else {
-            f.debug_list()
+            f.debug_map()
                 .entries(
-                    self.0
-                        .iter()
+                    collection
+                        .into_iter()
                         .take(LIMIT)
-                        .map(OrMore::Value)
-                        .chain([OrMore::More]),
+                        .map(|(k,v)|(OrMore::Value(k),OrMore::Value(v)))
+                        .chain([(OrMore::More,OrMore::More)])  
                 )
                 .finish()
         }
@@ -41,16 +43,16 @@ mod test {
     #[test]
     fn test_limited_output() {
         let w: LimitedHashMap<u32, u32> = LimitedHashMap(HashMap::new());
-        assert_eq!(format!("{w:?}"), "");
+        assert_eq!(format!("{w:?}"), "{}");
         let w = LimitedHashMap(HashMap::from([(1, 1)]));
-        assert_eq!(format!("{w:?}"), "");
+        assert_eq!(format!("{w:?}"), "{1: 1}");
         let w = LimitedHashMap(HashMap::from([(1, 1), (2, 2)]));
-        assert_eq!(format!("{w:?}"), "");
+        assert_eq!(format!("{w:?}"), "{1: 1, 2: 2}");
         let w = LimitedHashMap(HashMap::from([(1, 1), (2, 2), (3, 3)]));
-        assert_eq!(format!("{w:?}"), "");
+        assert_eq!(format!("{w:?}"), "{1: 1, 2: 2, 3: 3}");
         let w = LimitedHashMap(HashMap::from([(1, 1), (2, 2), (3, 3), (4, 4)]));
-        assert_eq!(format!("{w:?}"), "");
+        assert_eq!(format!("{w:?}"), "{1: 1, 2: 2, 3: 3, ...: ...}");
         let w = LimitedHashMap(HashMap::from([(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]));
-        assert_eq!(format!("{w:?}"), "");
+        assert_eq!(format!("{w:?}"), "{1: 1, 2: 2, 3: 3, ...: ...}");
     }
 }
