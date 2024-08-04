1662b70d9acd87b689c0d1e6200482aca78d1cfc
Frank Elsinga <frank@elsinga.de>
2024-08-04 20:37:07 +0200

    migrated partially to the exploded schema
    added a migration to explode the schema
    Necessary to migrate to a parquet based initialisation

--- server/src/build.rs
+++ server/src/build.rs
@@ -1,3 +1,21 @@
+use std::{env, fs, path::Path};
+
+use typify::{TypeSpace, TypeSpaceSettings};
+
 fn main() {
+    // sqlx
     println!("cargo:rerun-if-changed=migrations");
+    // types
+    let content = std::fs::read_to_string("../openapi.yaml").unwrap();
+    let schema = serde_json::from_str::<schemars::schema::RootSchema>(&content).unwrap();
+
+    let mut type_space = TypeSpace::new(TypeSpaceSettings::default().with_struct_builder(true));
+    type_space.add_root_schema(schema).unwrap();
+
+    let contents =
+        prettyplease::unparse(&syn::parse2::<syn::File>(type_space.to_stream()).unwrap());
+
+    let mut out_file = Path::new(&env::var("OUT_DIR").unwrap()).to_path_buf();
+    out_file.push("codegen.rs");
+    fs::write(out_file, contents).unwrap();
 }
