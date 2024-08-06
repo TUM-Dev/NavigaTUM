db45bbb2ed8f2355d2782789d8eadbaf88b4ba34
Frank Elsinga <frank@elsinga.de>
2024-08-06 03:54:08 +0200

    Update webclient/app/composables/webglSupport.ts

--- webclient/app/composables/webglSupport.ts
+++ webclient/app/composables/webglSupport.ts
@@ -2,7 +2,8 @@ function supportsWebgl(): boolean {
   try {
     const canvas = document.createElement("canvas");
     return !!window.WebGLRenderingContext && !!(canvas.getContext("webgl") || canvas.getContext("experimental-webgl"));
-  } catch (_e) {
+  } catch (e) {
+    console.error("cannot construct webglcontext (needed to render the map) because", e)
     return false;
   }
 }
