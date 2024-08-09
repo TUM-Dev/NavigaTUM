45c0881487989596681e826940ffae2d1b538cf3
Frank Elsinga <frank@elsinga.de>
2024-08-09 13:38:02 +0200

    removed unused methods

--- data/processors/maps/test_roomfinder.py
+++ data/processors/maps/test_roomfinder.py
@@ -4,7 +4,7 @@ import typing
 import pytest
 from external.models import roomfinder
 from processors.maps.models import Coordinate
-from processors.maps.roomfinder import _calc_xy_of_coords_on_map, _merge_maps, _merge_str
+from processors.maps.roomfinder import _calc_xy_of_coords_on_map
 
 
 def default_map(rotate: int = 0) -> roomfinder.Map:
@@ -25,75 +25,6 @@ def default_map(rotate: int = 0) -> roomfinder.Map:
         width=100,
         height=100,
     )
-
-
-@pytest.mark.parametrize("s_1", ["abc", " abc", "abc"])
-@pytest.mark.parametrize("s_2", ["abc", " abc", "abc"])
-def test_merge_identical(s_1: str, s_2: str) -> None:
-    """Test if identical inputs are stripped of whitespace"""
-    assert _merge_str(s_1, s_2) == "abc"
-
-
-def test_merge_strings_happy_path() -> None:
-    """Test if the merging of strings works as expected in the regular case"""
-    assert _merge_str("Thierschbau 5. OG", "Thierschbau 6. OG") == "Thierschbau 5/6. OG"
-    assert _merge_str("Pre Something Suf", "Pre Different Suf") == "Pre Something/Different Suf"
-    assert _merge_str("Hello World", "Hello Universe") == "Hello World/Universe"
-
-
-def test_merge_strings_subset() -> None:
-    """Test if the merging of strings works as expected when one string is a subset of the other"""
-    assert _merge_str("POSTFIX", "Another POSTFIX") == "(Another) POSTFIX"
-    assert _merge_str("Another POSTFIX", "POSTFIX") == "(Another) POSTFIX"
-    assert _merge_str("PREFIX", "PREFIX Another") == "PREFIX (Another)"
-    assert _merge_str("PREFIX Another", "PREFIX") == "PREFIX (Another)"
-
-
-def test_merge_maps() -> None:
-    """Test if the merging of maps works as expected"""
-    map1 = {
-        "id": 1,
-        "name": "prefix John posfix",
-        "age": 30,
-        "favourite_float": 3.0,
-        "location": {
-            "city": "New York",
-            "zip": 0,
-        },
-    }
-
-    map2 = {
-        "id": 2,
-        "name": "prefix Tod posfix",
-        "age": 25,
-        "favourite_float": 5.0,
-        "location": {
-            "city": "San Francisco",
-            "zip": 100,
-        },
-    }
-
-    expected_map = {
-        "id": 1,  # id should be taken from map1
-        "name": "prefix John/Tod posfix",  # merged string
-        "age": 27,  # average
-        "favourite_float": 4.0,  # average
-        "location": {
-            "city": "New York/San Francisco",  # merged string
-            "zip": 50,  # average
-        },
-    }
-
-    assert expected_map == _merge_maps(map1, map2)
-
-
-def test_merge_maps_unequal_keys() -> None:
-    """Test if the merging of maps works as expected when the keyspace is not equal"""
-    with pytest.raises(KeyError):
-        assert _merge_maps({"a": 1}, {"b": 2})  # different key in b
-    with pytest.raises(KeyError):
-        _merge_maps({"a": 1}, {})  # no key in b
-    assert _merge_maps({}, {"b": 2}) == {}  # no key in a => empty map
 
 
 @pytest.mark.parametrize("rotation", range(360))
