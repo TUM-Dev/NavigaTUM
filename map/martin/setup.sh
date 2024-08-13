dafb70b9e7d23fc3fc64d81e7dc31f60abd41452
Frank Elsinga <frank@elsinga.de>
2024-08-13 02:01:22 +0200

    made sure that quiet flags are used the same way across the project

--- map/martin/setup.sh
+++ map/martin/setup.sh
@@ -5,12 +5,12 @@ set -o errexit
 rm -rf $(dirname "$0")/fonts $(dirname "$0")/sprites
 
 # fonts
-wget -q -O /tmp/roboto-android.zip https://github.com/googlefonts/roboto/releases/download/v2.138/roboto-android.zip
-unzip /tmp/roboto-android.zip -d $(dirname "$0")/fonts/
+wget --quiet -O /tmp/roboto-android.zip https://github.com/googlefonts/roboto/releases/download/v2.138/roboto-android.zip
+unzip -q /tmp/roboto-android.zip -d $(dirname "$0")/fonts/
 rm /tmp/roboto-android.zip
 
 # sprites
-wget -q -O /tmp/sprites_maki.zip https://github.com/mapbox/maki/zipball/main
+wget --quiet -O /tmp/sprites_maki.zip https://github.com/mapbox/maki/zipball/main
 unzip -q /tmp/sprites_maki.zip -d /tmp/sprites/
 rm /tmp/sprites_maki.zip
 mkdir $(dirname "$0")/sprites/
