084c2895ec96f76c9e921f65e479f7be9a733a99
Frank Elsinga <frank@elsinga.de>
2024-08-14 18:02:18 +0200

    linting fixes

--- server/src/maps/indoor.rs
+++ server/src/maps/indoor.rs
@@ -6,7 +6,10 @@ use sqlx::{PgPool, Row};
 use tracing::{error, info};
 
 #[tracing::instrument(skip(pool))]
-pub async fn fetch_indoor_maps_inside_of(pool: &PgPool, geom: Geometry) -> anyhow::Result<Vec<i64>> {
+pub async fn fetch_indoor_maps_inside_of(
+    pool: &PgPool,
+    geom: Geometry,
+) -> anyhow::Result<Vec<i64>> {
     let filtered_groups = sqlx::query("SELECT group_id from indoor_features where ST_Contains(convex_hull::geometry, $1::geometry)")
         .bind(geozero::wkb::Encode(geom))
         .fetch_all(pool)
@@ -41,23 +44,22 @@ pub async fn get_indoor_map(
     data: web::Data<crate::AppData>,
 ) -> HttpResponse {
     let id = params.into_inner();
-    let map=fetch_indoor_map(&data.pool,id).await;
-    let geometry=match map {
+    let map = fetch_indoor_map(&data.pool, id).await;
+    let geometry = match map {
         Ok(g) => g,
         Err(err) => {
             error!("Failed to fetch indoor map {id} because {err:?}");
             return HttpResponse::InternalServerError().finish();
-        },
+        }
     };
     info!("fetched {geometry:?}");
     HttpResponse::Ok().finish()
 }
 
-
 #[derive(Serialize)]
-struct RemoteMap{
-    name:String,
-    url:Url,
+struct RemoteMap {
+    name: String,
+    url: Url,
 }
 
 #[post("/api/maps/indoor")]
@@ -65,22 +67,24 @@ pub async fn list_indoor_maps(
     web::Query(args): web::Query<Arguments>,
     data: web::Data<crate::AppData>,
 ) -> HttpResponse {
-    let maps=fetch_indoor_maps_inside_of(&data.pool,args.bbox.into()).await;
-    let maps=match maps{
-        Ok(m)=>m,
+    let maps = fetch_indoor_maps_inside_of(&data.pool, args.bbox.into()).await;
+    let maps = match maps {
+        Ok(m) => m,
         Err(e) => {
             error!("could not list maps because {e:?}");
-            return HttpResponse::InternalServerError().body("could not get indoor maps, please try again later");
-        },
+            return HttpResponse::InternalServerError()
+                .body("could not get indoor maps, please try again later");
+        }
     };
     let mut response = Vec::new();
-    for map in maps{
-        response.push(RemoteMap{
-            name: map.to_string(), 
-            url: format!("https://nav.tum.de/api/maps/indoor/{map}").parse().unwrap(),
+    for map in maps {
+        response.push(RemoteMap {
+            name: map.to_string(),
+            url: format!("https://nav.tum.de/api/maps/indoor/{map}")
+                .parse()
+                .unwrap(),
         })
     }
-    
+
     HttpResponse::Ok().json(response)
 }
-
