92e4ad1b0716df3887829d64cf2905102e4701c9
Frank Elsinga <frank@elsinga.de>
2024-08-23 21:55:47 +0200

    removed all down migrations as our strategy is to migrate foreward (we can throw away the db if it push comes to shove)

--- server/migrations/20230323144107_create_calendar.down.sql
+++
@@ -1,3 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP TABLE IF EXISTS calendar;
-DROP INDEX IF EXISTS calendar_lut;
--- server/migrations/20230603173046_new_table_rooms.down.sql
+++
@@ -1,2 +0,0 @@
--- This file should undo anything in `down.sql`
-DROP TABLE IF EXISTS rooms;
