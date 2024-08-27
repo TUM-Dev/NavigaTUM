04f2a59d8a3d99a0a3116f8fce2bf84b82ee2d4c
Frank Elsinga <frank@elsinga.de>
2024-08-27 17:10:53 +0200

    fixed the PK of `indoor_features` not allowing updates ^^

--- server/migrations/20240823194653_indoor_features_v2.sql
+++ server/migrations/20240823194653_indoor_features_v2.sql
@@ -1,14 +1,17 @@
 -- Add up migration script here
 -- indoor features v1 was removed as that version hard-depended on geodata being loaded.
--- indoor features v2 does not make this assumption
-
 DROP materialized VIEW IF EXISTS indoor_features CASCADE;
+
+-- indoor features v2 does not make this assumption
 CREATE TABLE indoor_features
 (
-    group_id bigint PRIMARY KEY NOT NULL UNIQUE,
+    group_id bigint not null,
     features     JSONB NOT NULL,
     convex_hull GEOMETRY NOT NULL,
     import_version bigint NOT NULL default 0
 );
+
+alter table indoor_features
+    add primary key (group_id, import_version);
 
 CREATE index indoor_features_hull_idx ON indoor_features USING GIST (convex_hull);
