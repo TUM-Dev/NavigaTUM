06c30c5cf973e817d04ec87be4f680deaf63795e
Frank Elsinga <frank@elsinga.de>
2024-09-25 01:24:14 +0800

    made sure that the sprites script works as intended instead of dying pittifully

--- map/martin/setup.sh
+++ map/martin/setup.sh
@@ -2,17 +2,18 @@
 
 set -o errexit
 
-rm -rf $(dirname "$0")/fonts $(dirname "$0")/sprites
+echo -- cleanup --
+rm -rf "$(dirname "$0")/fonts" "$(dirname "$0")/sprites" /tmp/sprites
 
-# fonts
+echo -- fonts --
 wget --quiet -O /tmp/roboto-android.zip https://github.com/googlefonts/roboto/releases/download/v2.138/roboto-android.zip
-unzip -q /tmp/roboto-android.zip -d $(dirname "$0")/fonts/
+unzip -q /tmp/roboto-android.zip -d "$(dirname "$0")/fonts/"
 rm /tmp/roboto-android.zip
 
-# sprites
+echo -- sprites --
 wget --quiet -O /tmp/sprites_maki.zip https://github.com/mapbox/maki/zipball/main
-unzip -q /tmp/sprites_maki.zip -d /tmp/sprites/
+mkdir --parents "$(dirname "$0")/sprites/maki/"
+unzip -q /tmp/sprites_maki.zip -d /tmp/sprites
 rm /tmp/sprites_maki.zip
-mkdir $(dirname "$0")/sprites/
-mv /tmp/sprites/mapbox-maki-*/icons/* $(dirname "$0")/sprites/maki/
+mv /tmp/sprites/mapbox-maki-*/icons/* "$(dirname "$0")/sprites/maki/"
 rm -fr /tmp/sprites/
