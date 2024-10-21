2905541e89e9b1ebce28f94504ccd53440442815
Frank Elsinga <frank@elsinga.de>
2024-10-21 14:04:43 +0200

    migrated the docker based download into a download script as the complexity is not managable otherwise

---
+++ map/download.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -o errexit
+
+cd "$(dirname "$0")"
+
+echo "-- cleanup $(dirname "$0") --"
+rm -rf ./data/* ./gtfs_feeds/*
+mkdir --parents gtfs_feeds
+mkdir --parents data/transit_tiles
+
+echo -- download dependencys --
+if command -v apk > /dev/null 2>&1
+then
+  apk --update add --quiet wget
+else
+  echo "[WARNING] skipping apk installation of wget. Please make sure it is installed"
+fi
+
+
+echo -- download geodata --
+cd data || exit 1
+wget "https://download.geofabrik.de/$1-latest.osm.pbf" --tries=5 --random-wait --wait=5
+cd .. || exit 1
+
+echo -- download gtfs feeds --
+cd gtfs_feeds || exit 1
+# aah, MVV, warum machst du so Sachen???
+# TODO: look into https://www.delfi.de/de/leistungen-produkte/daten-dienste/
+# TODO: look into https://www.transit.land/feeds?search=germany
+# TODO: look into https://github.com/transitland/transitland-atlas/pull/1268
+wget --tries=5 https://www.opendata-oepnv.de/dataset/17065229-c3fd-46d7-84a9-aae55aadbf40/resource/927d0830-2a40-4702-acc6-f5716352b666/download/gtfs_mvv_mitshape_240814.zip --output-document mvv.zip --tries=5 --random-wait --wait=5
+unzip -q -d mvv mvv.zip
+rm mvv.zip
+cd .. || exit 1
