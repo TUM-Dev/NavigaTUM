007dc137e1f2d95b7f7240add5184e5301ca654e
Frank Elsinga <frank@elsinga.de>
2024-11-08 03:05:05 +0100

    added a calendar short url

---
+++ webclient/app/pages/calendar/[id].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+onMounted(async () => {
+  const route = useRoute();
+  const localePath = useLocalePath();
+  await navigateTo({
+    path: localePath(`/view/${route.params.id}`),
+    query: { "calendar[]": route.params.id },
+    replace: true,
+  });
+});
+</script>
