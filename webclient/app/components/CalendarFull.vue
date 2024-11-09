280ee21ad9a43b0d724e50de3490edd20cd4c8e9
Frank Elsinga <frank@elsinga.de>
2024-11-09 12:58:46 +0100

    reduced the opacity of the `timeText` for better hinting

--- webclient/app/components/CalendarFull.vue
+++ webclient/app/components/CalendarFull.vue
@@ -146,7 +146,7 @@ function refetchEvents() {
             >
           </template>
           <template v-else>
-            <span class="font-normal">{{ arg.timeText }}</span>
+            <span class="font-normal opacity-70">{{ arg.timeText }}</span>
             <span class="font-medium">{{ arg.event.title }}</span>
           </template>
         </NuxtLink>
