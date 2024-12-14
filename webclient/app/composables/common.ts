9789cef4376743d3af4284a40cb6d249ce394a19
Frank Elsinga <frank@elsinga.de>
2024-12-14 02:44:33 +0100

    moved `firstOrDefault` to a composable

--- webclient/app/composables/common.ts
+++ webclient/app/composables/common.ts
@@ -1,8 +1,19 @@
+import type { LocationQueryValue } from "#vue-router";
+
 export function setTitle(name: string): void {
   document.title = `${name} – NavigaTUM`;
   document.querySelector('meta[property="og:title"]')?.setAttribute("content", name);
 }
+
 export function setDescription(description: string): void {
   document.querySelector('meta[name="description"]')?.setAttribute("content", description);
   document.querySelector('meta[property="og:description"]')?.setAttribute("content", description);
+}
+
+export function firstOrDefault(
+  value: LocationQueryValue | LocationQueryValue[] | undefined,
+  defaultValue: string,
+): string {
+  if (Array.isArray(value)) return value[0] ?? defaultValue;
+  return value ?? defaultValue;
 }
