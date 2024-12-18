4c009fe5b921332e428bd3d1bdef694070f3e603
Frank Elsinga <frank@elsinga.de>
2024-12-18 17:20:32 +0100

    chore: removed dead code

--- webclient/app/composables/common.ts
+++ webclient/app/composables/common.ts
@@ -1,15 +1,5 @@
 import type { LocationQueryValue } from "#vue-router";
 
-export function setTitle(name: string): void {
-  document.title = `${name} – NavigaTUM`;
-  document.querySelector('meta[property="og:title"]')?.setAttribute("content", name);
-}
-
-export function setDescription(description: string): void {
-  document.querySelector('meta[name="description"]')?.setAttribute("content", description);
-  document.querySelector('meta[property="og:description"]')?.setAttribute("content", description);
-}
-
 export function firstOrDefault(
   value: LocationQueryValue | LocationQueryValue[] | undefined,
   defaultValue: string,
