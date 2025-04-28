a3051a6caf81f77004c364af2a22a398858bc691
Frank Elsinga <frank@elsinga.de>
2025-04-29 01:30:36 +0200

    change the ShareIcon to be the larger variant

--- webclient/app/components/ShareButton.vue
+++ webclient/app/components/ShareButton.vue
@@ -2,7 +2,7 @@
 import type { UseShareOptions } from "@vueuse/core";
 import { useClipboard, useShare } from "@vueuse/core";
 import type { components } from "~/api_types";
-import { ShareIcon } from "@heroicons/vue/16/solid";
+import { ShareIcon } from "@heroicons/vue/24/outline";
 import { ClipboardDocumentCheckIcon, ClipboardIcon } from "@heroicons/vue/20/solid";
 
 const props = defineProps<{
