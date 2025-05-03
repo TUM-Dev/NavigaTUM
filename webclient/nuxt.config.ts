c6795f2dc0fa953e9caedf3f991446672340af1c
Frank Elsinga <frank@elsinga.de>
2025-05-03 15:09:28 +0200

    update the compatabilityDate

--- webclient/nuxt.config.ts
+++ webclient/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-10-29",
+  compatibilityDate: "2025-04-30",
   future: {
     compatibilityVersion: 4,
   },
