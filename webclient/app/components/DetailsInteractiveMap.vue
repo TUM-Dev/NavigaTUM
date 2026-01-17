076a9004f512cee89c6f5bd2c13dcc3857b0e7d3
Frank Elsinga <frank@elsinga.de>
2026-01-17 19:49:13 +0100

    fix setting validateStyle to false in dev

--- webclient/app/components/DetailsInteractiveMap.vue
+++ webclient/app/components/DetailsInteractiveMap.vue
@@ -101,7 +101,7 @@ function initMap(containerId: string): MapLibreMap {
 
     center: [11.5748, 48.14], // Approx Munich
     zoom: 11, // Zoomed out so that the whole city is visible
-    validateStyle: false,
+    validateStyle: import.meta.env.PROD,
     maplibreLogo: true,
   });
 
